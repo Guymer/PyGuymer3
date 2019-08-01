70e34f4482efe12d6033825d0162cb1a59d62230
Thomas Guymer <t.m.guymer@thomasguymer.co.uk>
2019-08-01 17:19:45 +0100

    style change

--- find_program_version.py
+++ find_program_version.py
@@ -5,78 +5,31 @@ def find_program_version(prog = "python"):
     import subprocess
 
     # Check if it is FreeBSD ...
-    proc = subprocess.Popen(
-        [
-            "type",
-            "pkg"
-        ],
-        stderr = open(os.devnull, "wt"),
-        stdout = open(os.devnull, "wt")
-    )
+    proc = subprocess.Popen(["type", "pkg"], stderr = open(os.devnull, "wt"), stdout = open(os.devnull, "wt"))
     proc.wait()
     if proc.returncode == 0:
         # Find raw string ...
-        proc = subprocess.Popen(
-            [
-                "pkg",
-                "info",
-                prog
-            ],
-            stderr = subprocess.PIPE,
-            stdout = subprocess.PIPE
-        )
+        proc = subprocess.Popen(["pkg", "info", prog], stderr = subprocess.PIPE, stdout = subprocess.PIPE)
         stdout, stderr = proc.communicate()
         if proc.returncode != 0:
             raise Exception("\"pkg\" command failed")
     else:
         # Check if it is MacPorts ...
-        proc = subprocess.Popen(
-            [
-                "type",
-                "port"
-            ],
-            stderr = open(os.devnull, "wt"),
-            stdout = open(os.devnull, "wt")
-        )
+        proc = subprocess.Popen(["type", "port"], stderr = open(os.devnull, "wt"), stdout = open(os.devnull, "wt"))
         proc.wait()
         if proc.returncode == 0:
             # Find raw string ...
-            proc = subprocess.Popen(
-                [
-                    "port",
-                    "info",
-                    "--version",
-                    prog
-                ],
-                stderr = subprocess.PIPE,
-                stdout = subprocess.PIPE
-            )
+            proc = subprocess.Popen(["port", "info", "--version", prog], stderr = subprocess.PIPE, stdout = subprocess.PIPE)
             stdout, stderr = proc.communicate()
             if proc.returncode != 0:
                 raise Exception("\"port\" command failed")
         else:
             # Check if it is OpenSUSE ...
-            proc = subprocess.Popen(
-                [
-                    "type",
-                    "zypper"
-                ],
-                stderr = open(os.devnull, "wt"),
-                stdout = open(os.devnull, "wt")
-            )
+            proc = subprocess.Popen(["type", "zypper"], stderr = open(os.devnull, "wt"), stdout = open(os.devnull, "wt"))
             proc.wait()
             if proc.returncode == 0:
                 # Find raw string ...
-                proc = subprocess.Popen(
-                    [
-                        "zypper",
-                        "--disable-repositories",
-                        "info",
-                        prog
-                    ],
-                    stderr = subprocess.PIPE,
-                    stdout = subprocess.PIPE
-                )
+                proc = subprocess.Popen(["zypper", "--disable-repositories", "info", prog], stderr = subprocess.PIPE, stdout = subprocess.PIPE)
                 stdout, stderr = proc.communicate()
                 if proc.returncode != 0:
                     raise Exception("\"zypper\" command failed")
