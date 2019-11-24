5f58fff575c248ef5379790b83e0236366a661d5
Thomas Guymer <t.m.guymer@thomasguymer.co.uk>
2019-11-24 11:18:42 +0000

    removed duplicate file

--- parse_CLPI_file.py
+++ parse_CLPI_file.py
@@ -1,9 +1,7 @@
 def parse_CLPI_file(br, ip):
     # Import modules ...
     import os
-    import pyguymer3.MPLS
-
-    print("WARNING: parse_CLPI_file() is experimental")
+    import pyguymer3.CLPI
 
     # Create dictionary to hold information ...
     info = {}
@@ -11,29 +9,41 @@ def parse_CLPI_file(br, ip):
     # Open file ...
     with open(os.path.join(br, "BDMV/CLIPINF/{0:05d}.clpi".format(ip)), "rb") as fobj:
         # Load header ...
-        res, length0 = pyguymer3.MPLS.load_header(fobj)
+        res = pyguymer3.CLPI.load_header(fobj)
         info["header"] = res
 
-        # Load AppInfoPlayList section ...
-        res, length1 = pyguymer3.MPLS.load_AppInfoPlayList(fobj)
-        info["AppInfoPlayList"] = res
+        # Load ClipInfo section ...
+        res = pyguymer3.CLPI.load_ClipInfo(fobj)
+        info["ClipInfo"] = res
 
-        # Load PlayList section ...
-        if info["header"]["PlayListStartAddress"] != 0:
-            fobj.seek(info["header"]["PlayListStartAddress"], os.SEEK_SET)
-            res, length2 = pyguymer3.MPLS.load_PlayList(fobj)
-            info["PlayList"] = res
+        # Load SequenceInfo section ...
+        if info["header"]["SequenceInfoStartAddress"] != 0:
+            fobj.seek(info["header"]["SequenceInfoStartAddress"], os.SEEK_SET)
+            res = pyguymer3.CLPI.load_SequenceInfo(fobj)
+            info["SequenceInfo"] = res
 
-        # Load PlayListMark section ...
-        if info["header"]["PlayListMarkStartAddress"] != 0:
-            fobj.seek(info["header"]["PlayListMarkStartAddress"], os.SEEK_SET)
-            res, length3 = pyguymer3.MPLS.load_PlayListMark(fobj)
-            info["PlayListMark"] = res
+        # Load ProgramInfo section ...
+        if info["header"]["ProgramInfoStartAddress"] != 0:
+            fobj.seek(info["header"]["ProgramInfoStartAddress"], os.SEEK_SET)
+            res = pyguymer3.CLPI.load_ProgramInfo(fobj)
+            info["ProgramInfo"] = res
+
+        # Load CPI section ...
+        if info["header"]["CPIStartAddress"] != 0:
+            fobj.seek(info["header"]["CPIStartAddress"], os.SEEK_SET)
+            res = pyguymer3.CLPI.load_CPI(fobj)
+            info["CPI"] = res
+
+        # Load ClipMark section ...
+        if info["header"]["ClipMarkStartAddress"] != 0:
+            fobj.seek(info["header"]["ClipMarkStartAddress"], os.SEEK_SET)
+            res = pyguymer3.CLPI.load_ClipMark(fobj)
+            info["ClipMark"] = res
 
         # Load ExtensionData section ...
         if info["header"]["ExtensionDataStartAddress"] != 0:
             fobj.seek(info["header"]["ExtensionDataStartAddress"], os.SEEK_SET)
-            res, length4 = pyguymer3.MPLS.load_ExtensionData(fobj)
+            res = pyguymer3.CLPI.load_ExtensionData(fobj)
             info["ExtensionData"] = res
 
     # Return answer ...
