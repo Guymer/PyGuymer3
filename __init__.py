4966d3ba0e213c45f4d734aed1c9d5d2a93550ab
Thomas Guymer <t.m.guymer@thomasguymer.co.uk>
2019-11-29 12:39:53 +0000

    added function to start default session

--- __init__.py
+++ __init__.py
@@ -78,6 +78,7 @@ from .save_array_as_PPM import save_array_as_PPM
 from .save_array_as_image import save_array_as_image
 from .save_file_if_needed import save_file_if_needed
 from .simplify_poly import simplify_poly
+from .start_session import start_session
 from .yuv2rgb import yuv2rgb
 
 # Ensure that this module is only imported by Python 3.x ...
---
+++ start_session.py
@@ -0,0 +1,20 @@
+def start_session():
+    # Import special modules ...
+    try:
+        import requests
+    except:
+        raise Exception("\"requests\" is not installed; run \"pip install --user requests\"")
+
+    # Start session ...
+    sess = requests.Session()
+    sess.allow_redirects = True
+    sess.headers.update({"Accept" : "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"})
+    sess.headers.update({"Accept-Language" : "en-GB,en;q=0.5"})
+    sess.headers.update({"Accept-Encoding" : "gzip, deflate"})
+    sess.headers.update({"DNT" : "1"})
+    sess.headers.update({"Upgrade-Insecure-Requests" : "1"})
+    sess.headers.update({"User-Agent" : "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:68.0) Gecko/20100101 Firefox/68.0"})
+    sess.max_redirects = 5
+
+    # Return answer ...
+    return sess
