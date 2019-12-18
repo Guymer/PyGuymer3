135c58521d3c54236bb8231a3e2684fd09a06319
Thomas Guymer <t.m.guymer@thomasguymer.co.uk>
2019-12-18 09:37:24 +0000

    now parses all the way down to Modification-Time and replaces it with 0 when it is passed to the hash object

--- return_hash_of_MP4.py
+++ return_hash_of_MP4.py
@@ -1,4 +1,12 @@
 def return_hash_of_MP4(fname = "missing"):
+    """
+    This function returns the SHA512 hash of the passed MP4 file as if the
+    "Modification Time" field (in the "mvhd" atom in the "moov" atom) is set to
+    zero. Using this function it is possible to discover that the only binary
+    difference between two different MP4 files is the "Modification Time" field
+    (in the "mvhd" atom in the "moov" atom).
+    """
+
     # NOTE: The following websites have some very useful information on how to
     #       parse MP4 files - the first just forgot to say that integers are
     #       big-endian.
@@ -27,7 +35,7 @@ def return_hash_of_MP4(fname = "missing"):
 
         # Loop over entire contents of MP4 ...
         while fobj.tell() < fsize:
-            # Attempt to read 4 bytes as a big-endian un-signed 32 bit integer
+            # Attempt to read 4 bytes as a big-endian un-signed 32-bit integer
             # and pass them to the hash object ...
             src = fobj.read(4)
             val, = struct.unpack(">I", src)                                     # [B]
@@ -50,10 +58,6 @@ def return_hash_of_MP4(fname = "missing"):
             else:
                 foundFTYP = True
 
-            # Check if it is the MOOV atom ...
-            if name == "moov":
-                foundMOOV = True
-
             # Check the length ...
             if val == 0:
                 # NOTE: This atom runs until EOF.
@@ -66,19 +70,94 @@ def return_hash_of_MP4(fname = "missing"):
             elif val == 1:
                 # NOTE: This atom has 64-bit sizes.
 
-                # Attempt to read 8 bytes as a big-endian un-signed 64 bit
+                # Attempt to read 8 bytes as a big-endian un-signed 64-bit
                 # integer and pass them to the hash object ...
                 src = fobj.read(8)
                 val, = struct.unpack(">Q", src)                                 # [B]
                 hobj.update(src)
                 off += 8
 
-            # Pass the rest of the atom to the hash object ...
-            hobj.update(fobj.read(val - off))
+            # Create short-hand ...
+            rem = val - off
+
+            # Check if it is the MOOV atom ...
+            if name == "moov":
+                # Set trigger ...
+                foundMOOV = True
+
+                # Save starting position ...
+                pos = fobj.tell()
+
+                # Loop over remaining contents of MOOV atom ...
+                while fobj.tell() - pos < rem:
+                    # Attempt to read 4 bytes as a big-endian un-signed 32-bit
+                    # integer and pass them to the hash object ...
+                    src = fobj.read(4)
+                    val, = struct.unpack(">I", src)                             # [B]
+                    hobj.update(src)
+                    off2 = 4
+
+                    # Extract atom name and pass it to the hash object ...
+                    src = fobj.read(4)
+                    name = src.decode("utf-8")
+                    hobj.update(src)
+                    off2 += 4
+
+                    # Check that it matches the pattern ...
+                    if re.match(r"[a-z][a-z][a-z][a-z]", name) is None:
+                        raise Exception("\"{0:s}\" is not an atom name in \"{1:s}\"".format(name, fname))
+
+                    # Check the length ...
+                    if val == 0:
+                        # NOTE: This atom runs until EOF.
+
+                        # Pass the rest of the atom to the hash object ...
+                        hobj.update(fobj.read())
+
+                        # Stop looping ...
+                        break
+                    elif val == 1:
+                        # NOTE: This atom has 64-bit sizes.
+
+                        # Attempt to read 8 bytes as a big-endian un-signed
+                        # 64-bit integer and pass them to the hash object ...
+                        src = fobj.read(8)
+                        val, = struct.unpack(">Q", src)                         # [B]
+                        hobj.update(src)
+                        off2 += 8
+
+                    # Create short-hand ...
+                    rem2 = val - off
+
+                    # Check if it is the MVHD atom ...
+                    if name == "mvhd":
+                        # Set trigger ...
+                        foundMVHD = True
+
+                        # Check that it is the correct size ...
+                        if rem2 != 100:
+                            raise Exception("the \"mvhd\" atom in \"{0:s}\" is not the correct size".format(fname))
+
+                        # Pass the rest of the atom to the hash object (except
+                        # the "Modification Time" for which instead pass 0 as a
+                        # big-endian un-signed 32-bit integer) ...
+                        # NOTE: See Figure 2-3 of https://developer.apple.com/library/archive/documentation/QuickTime/QTFF/QTFFChap2/qtff2.html
+                        hobj.update(fobj.read(8))
+                        fobj.read(4)
+                        hobj.update(struct.pack(">I", 0))
+                        hobj.update(fobj.read(88))
+                    else:
+                        # Pass the rest of the atom to the hash object ...
+                        hobj.update(fobj.read(rem2))
+            else:
+                # Pass the rest of the atom to the hash object ...
+                hobj.update(fobj.read(rem))
 
     # Catch possible errors ...
     if not foundMOOV:
         raise Exception("did not find moov atom in \"{0:s}\"".format(fname))
+    if not foundMVHD:
+        raise Exception("did not find mvhd atom in \"{0:s}\"".format(fname))
 
     # Return hash hexdigest ...
     return hobj.hexdigest()
