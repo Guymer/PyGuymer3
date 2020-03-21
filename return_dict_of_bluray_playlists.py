49f4c1a85217835efbfbc2cda84ecbf87e1ee0bf
Thomas Guymer <t.m.guymer@thomasguymer.co.uk>
2020-03-21 16:47:31 +0000

    save results from ffprobe in global dictionary to speed up subsequent calls

--- return_dict_of_bluray_playlists.py
+++ return_dict_of_bluray_playlists.py
@@ -1,48 +1,38 @@
 def return_dict_of_bluray_playlists(dname, size_threshold = 1073741824, time_threshold = 60.0):
     # Import modules ...
     import glob
-    import json
     import os
-    import subprocess
 
-    # Create empty dictionary ...
+    # Load sub-functions ...
+    from . import __ffprobe
+    from .ffprobe import ffprobe
+
+    # Create short-hand ...
+    fname = "bluray:{:s}".format(dname)
+
+    # Initialize dictionary ...
     ans = {}
 
-    # Loop over playlists ...
-    for fname in glob.glob(os.path.join(dname, "BDMV", "PLAYLIST", "*.mpls")):
-        # Extract number ...
-        playlist = int(os.path.basename(fname).split(".")[0])
+    # Loop over playlist files ...
+    for mpls in glob.glob(os.path.join(dname, "BDMV", "PLAYLIST", "*.mpls")):
+        # Extract playlist number ...
+        playlist = int(os.path.basename(mpls).split(".")[0])
 
-        # Find format info ...
-        proc = subprocess.Popen(
-            [
-                "ffprobe",
-                "-loglevel", "quiet",
-                "-probesize", "3G",
-                "-analyzeduration", "1800M",
-                "-print_format", "json",
-                "-show_format",
-                "-playlist", "{:d}".format(playlist),
-                "bluray:{:s}".format(dname)
-            ],
-            encoding = "utf-8",
-            stderr = subprocess.PIPE,
-            stdout = subprocess.PIPE
-        )
-        stdout, stderr = proc.communicate()
-        if proc.returncode != 0:
-            print("WARNING: \"ffprobe\" command failed on playlist \"{:d}\" of \"{:s}\"".format(playlist, fname))
-            continue
+        # Make sure that this fname/playlist combination is in the global
+        # dictionary ...
+        if fname not in __ffprobe:
+            __ffprobe[fname] = {}
+        if playlist not in __ffprobe[fname]:
+            __ffprobe[fname][playlist] = ffprobe(fname, playlist)
 
         # Append information if this playlist is worthwhile (by default,
         # "worthwhile" is defined as >=1 GiB and/or >=1 minute) ...
-        info = json.loads(stdout)["format"]
-        if "duration" in info:
-            if float(info["duration"]) >= time_threshold:
-                ans["{:d}".format(playlist)] = info
-        if "size" in info:
-            if int(info["size"]) >= size_threshold:
-                ans["{:d}".format(playlist)] = info
+        if "duration" in __ffprobe[fname][playlist]["format"]:
+            if float(__ffprobe[fname][playlist]["format"]["duration"]) >= time_threshold:
+                ans["{:d}".format(playlist)] = __ffprobe[fname][playlist]["format"]
+        if "size" in __ffprobe[fname][playlist]["format"]:
+            if int(__ffprobe[fname][playlist]["format"]["size"]) >= size_threshold:
+                ans["{:d}".format(playlist)] = __ffprobe[fname][playlist]["format"]
 
     # Return dictionary ...
     return ans
