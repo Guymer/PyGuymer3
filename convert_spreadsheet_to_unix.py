c8f3f1e9e2a21362da47205006aebd25eb915a2e
Thomas Guymer <t.m.guymer@thomasguymer.co.uk>
2020-05-23 10:27:28 +0100

    added documentation

--- convert_spreadsheet_to_unix.py
+++ convert_spreadsheet_to_unix.py
@@ -1,2 +1,9 @@
 def convert_spreadsheet_to_unix(val):
+    """
+    This function converts a date from an Excel spreadsheet (which is an
+    integer) into the number of seconds since the Unix epoch (which is also an
+    integer).
+    """
+
+    # Return answer ..
     return 86400 * (val - 25569)
