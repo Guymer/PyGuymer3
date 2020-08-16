d6d6fc62f2b4acb2f1ff7abc195cc5b91e346cdf
Thomas Guymer <t.m.guymer@thomasguymer.co.uk>
2020-08-16 16:22:59 +0100

    added checks for file size and file hashes

--- gifsicle.py
+++ gifsicle.py
@@ -1,4 +1,4 @@
-def gifsicle(fname):
+def gifsicle(fname1, debug = False):
     """
     "gifsicle" does modify, and it does touch, the image if it cannot make
     it smaller, therefore it is NOT safe to keep on running "gifsicle" on the
@@ -14,7 +14,12 @@ def gifsicle(fname):
 
     # Import standard modules ...
     import os
+    import shutil
     import subprocess
+    import tempfile
+
+    # Load sub-functions ...
+    from .sha512 import sha512
 
     # Check that "gifsicle" is installed ...
     try:
@@ -31,22 +36,45 @@ def gifsicle(fname):
         raise Exception("\"gifsicle\" is not installed")
 
     # Check that the image exists ...
-    if not os.path.exists(fname):
-        raise Exception("\"{:s}\" does not exist".format(fname))
+    if not os.path.exists(fname1):
+        raise Exception("\"{:s}\" does not exist".format(fname1))
 
-    # Optimise GIF ...
-    try:
-        subprocess.check_call(
-            [
-                "gifsicle",
-                "--batch",
-                "--unoptimize",
-                "--optimize=3",
-                fname
-            ],
-            encoding = "utf-8",
-            stdout = open(os.devnull, "wt"),
-            stderr = open(os.devnull, "wt")
-        )
-    except subprocess.CalledProcessError:
-        raise Exception("\"gifsicle\" failed")
+    # Create temporary directory ...
+    with tempfile.TemporaryDirectory(prefix = "jpegtran.") as tname:
+        # Create temporary name ...
+        fname2 = os.path.join(tname, "image.gif")
+
+        # Optimise GIF ...
+        try:
+            subprocess.check_call(
+                [
+                    "gifsicle",
+                    "--batch",
+                    "--unoptimize",
+                    "--optimize=3",
+                    "--output", fname2,
+                    fname1
+                ],
+                encoding = "utf-8",
+                stdout = open(os.devnull, "wt"),
+                stderr = open(os.devnull, "wt")
+            )
+        except subprocess.CalledProcessError:
+            raise Exception("\"gifsicle\" failed")
+
+        # Find the two sizes and don't replace the original if the new one is
+        # larger, or equal ...
+        if os.path.getsize(fname2) >= os.path.getsize(fname1):
+            if debug:
+                print("INFO: Skipping because \"{:s}\" is larger than, or equal to, \"{:s}\"".format(fname2, fname1))
+            return
+
+        # Find the two hashes and don't replace the original if the new one is
+        # the same ...
+        if sha512(fname1).hexdigest() == sha512(fname2).hexdigest():
+            if debug:
+                print("INFO: Skipping because \"{:s}\" is the same as \"{:s}\"".format(fname2, fname1))
+            return
+
+        # Replace the original ...
+        shutil.move(fname2, fname1)
--- jpegtran.py
+++ jpegtran.py
@@ -1,4 +1,4 @@
-def jpegtran(fname):
+def jpegtran(fname1, debug = False):
     """
     "jpegtran" does not modify, but it does touch, the image if it cannot make
     it smaller, therefore it is NOT safe to keep on running "jpegtran" on the
@@ -7,7 +7,12 @@ def jpegtran(fname):
 
     # Import standard modules ...
     import os
+    import shutil
     import subprocess
+    import tempfile
+
+    # Load sub-functions ...
+    from .sha512 import sha512
 
     # Check that "jpegtran" is installed ...
     try:
@@ -24,23 +29,45 @@ def jpegtran(fname):
         raise Exception("\"jpegtran\" is not installed")
 
     # Check that the image exists ...
-    if not os.path.exists(fname):
-        raise Exception("\"{0:s}\" does not exist".format(fname))
+    if not os.path.exists(fname1):
+        raise Exception("\"{0:s}\" does not exist".format(fname1))
 
-    # Optimise JP[E]G ...
-    try:
-        subprocess.check_call(
-            [
-                "jpegtran",
-                "-copy", "all",
-                "-optimise",
-                "-outfile", fname,
-                "-perfect",
-                fname
-            ],
-            encoding = "utf-8",
-            stdout = open(os.devnull, "wt"),
-            stderr = open(os.devnull, "wt")
-        )
-    except subprocess.CalledProcessError:
-        raise Exception("\"jpegtran\" failed")
+    # Create temporary directory ...
+    with tempfile.TemporaryDirectory(prefix = "jpegtran.") as tname:
+        # Create temporary name ...
+        fname2 = os.path.join(tname, "image.jpg")
+
+        # Optimise JP[E]G ...
+        try:
+            subprocess.check_call(
+                [
+                    "jpegtran",
+                    "-copy", "all",
+                    "-optimise",
+                    "-outfile", fname2,
+                    "-perfect",
+                    fname1
+                ],
+                encoding = "utf-8",
+                stdout = open(os.devnull, "wt"),
+                stderr = open(os.devnull, "wt")
+            )
+        except subprocess.CalledProcessError:
+            raise Exception("\"jpegtran\" failed")
+
+        # Find the two sizes and don't replace the original if the new one is
+        # larger, or equal ...
+        if os.path.getsize(fname2) >= os.path.getsize(fname1):
+            if debug:
+                print("INFO: Skipping because \"{:s}\" is larger than, or equal to, \"{:s}\"".format(fname2, fname1))
+            return
+
+        # Find the two hashes and don't replace the original if the new one is
+        # the same ...
+        if sha512(fname1).hexdigest() == sha512(fname2).hexdigest():
+            if debug:
+                print("INFO: Skipping because \"{:s}\" is the same as \"{:s}\"".format(fname2, fname1))
+            return
+
+        # Replace the original ...
+        shutil.move(fname2, fname1)
