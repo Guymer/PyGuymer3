a8179e6c4d2373aa512a9c6848274633cfaace41
Thomas Guymer <t.m.guymer@thomasguymer.co.uk>
2020-09-20 20:25:23 +0100

    bugfix: corrected importing

--- parse_CLPI_file.py
+++ parse_CLPI_file.py
@@ -3,7 +3,13 @@ def parse_CLPI_file(br, ip):
     import os
 
     # Load sub-functions ...
-    from .CLPI import CLPI
+    from .CLPI.load_header import load_header
+    from .CLPI.load_ClipInfo import load_ClipInfo
+    from .CLPI.load_ClipMark import load_ClipMark
+    from .CLPI.load_CPI import load_CPI
+    from .CLPI.load_ExtensionData import load_ExtensionData
+    from .CLPI.load_ProgramInfo import load_ProgramInfo
+    from .CLPI.load_SequenceInfo import load_SequenceInfo
 
     # Create dictionary to hold information ...
     info = {}
@@ -11,41 +17,41 @@ def parse_CLPI_file(br, ip):
     # Open file ...
     with open(os.path.join(br, "BDMV/CLIPINF/{0:05d}.clpi".format(ip)), "rb") as fobj:
         # Load header ...
-        res = CLPI.load_header(fobj)
+        res = load_header(fobj)
         info["header"] = res
 
         # Load ClipInfo section ...
-        res = CLPI.load_ClipInfo(fobj)
+        res = load_ClipInfo(fobj)
         info["ClipInfo"] = res
 
         # Load SequenceInfo section ...
         if info["header"]["SequenceInfoStartAddress"] != 0:
             fobj.seek(info["header"]["SequenceInfoStartAddress"], os.SEEK_SET)
-            res = CLPI.load_SequenceInfo(fobj)
+            res = load_SequenceInfo(fobj)
             info["SequenceInfo"] = res
 
         # Load ProgramInfo section ...
         if info["header"]["ProgramInfoStartAddress"] != 0:
             fobj.seek(info["header"]["ProgramInfoStartAddress"], os.SEEK_SET)
-            res = CLPI.load_ProgramInfo(fobj)
+            res = load_ProgramInfo(fobj)
             info["ProgramInfo"] = res
 
         # Load CPI section ...
         if info["header"]["CPIStartAddress"] != 0:
             fobj.seek(info["header"]["CPIStartAddress"], os.SEEK_SET)
-            res = CLPI.load_CPI(fobj)
+            res = load_CPI(fobj)
             info["CPI"] = res
 
         # Load ClipMark section ...
         if info["header"]["ClipMarkStartAddress"] != 0:
             fobj.seek(info["header"]["ClipMarkStartAddress"], os.SEEK_SET)
-            res = CLPI.load_ClipMark(fobj)
+            res = load_ClipMark(fobj)
             info["ClipMark"] = res
 
         # Load ExtensionData section ...
         if info["header"]["ExtensionDataStartAddress"] != 0:
             fobj.seek(info["header"]["ExtensionDataStartAddress"], os.SEEK_SET)
-            res = CLPI.load_ExtensionData(fobj)
+            res = load_ExtensionData(fobj)
             info["ExtensionData"] = res
 
     # Return answer ...
--- parse_MPLS_file.py
+++ parse_MPLS_file.py
@@ -3,7 +3,11 @@ def parse_MPLS_file(br, ip):
     import os
 
     # Load sub-functions ...
-    from .MPLS import MPLS
+    from .MPLS.load_header import load_header
+    from .MPLS.load_AppInfoPlayList import load_AppInfoPlayList
+    from .MPLS.load_ExtensionData import load_ExtensionData
+    from .MPLS.load_PlayList import load_PlayList
+    from .MPLS.load_PlayListMark import load_PlayListMark
 
     # Create dictionary to hold information ...
     info = {}
@@ -11,29 +15,29 @@ def parse_MPLS_file(br, ip):
     # Open file ...
     with open(os.path.join(br, "BDMV/PLAYLIST/{0:05d}.mpls".format(ip)), "rb") as fobj:
         # Load header ...
-        res, length0 = MPLS.load_header(fobj)
+        res, length0 = load_header(fobj)
         info["header"] = res
 
         # Load AppInfoPlayList section ...
-        res, length1 = MPLS.load_AppInfoPlayList(fobj)
+        res, length1 = load_AppInfoPlayList(fobj)
         info["AppInfoPlayList"] = res
 
         # Load PlayList section ...
         if info["header"]["PlayListStartAddress"] != 0:
             fobj.seek(info["header"]["PlayListStartAddress"], os.SEEK_SET)
-            res, length2 = MPLS.load_PlayList(fobj)
+            res, length2 = load_PlayList(fobj)
             info["PlayList"] = res
 
         # Load PlayListMark section ...
         if info["header"]["PlayListMarkStartAddress"] != 0:
             fobj.seek(info["header"]["PlayListMarkStartAddress"], os.SEEK_SET)
-            res, length3 = MPLS.load_PlayListMark(fobj)
+            res, length3 = load_PlayListMark(fobj)
             info["PlayListMark"] = res
 
         # Load ExtensionData section ...
         if info["header"]["ExtensionDataStartAddress"] != 0:
             fobj.seek(info["header"]["ExtensionDataStartAddress"], os.SEEK_SET)
-            res, length4 = MPLS.load_ExtensionData(fobj)
+            res, length4 = load_ExtensionData(fobj)
             info["ExtensionData"] = res
 
     # Return answer ...
