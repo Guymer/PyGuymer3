24915d3d0f2198a53d3bb9750dd612fd3f7f1c6c
Thomas Guymer <t.m.guymer@thomasguymer.co.uk>
2020-10-11 08:22:42 +0100

    make indent available to the end-user

--- parse_MPLS_file.py
+++ parse_MPLS_file.py
@@ -1,4 +1,4 @@
-def parse_MPLS_file(br, ip, debug = False, errors = "strict"):
+def parse_MPLS_file(br, ip, debug = False, errors = "strict", indent = 0):
     # Import standard modules ...
     import os
 
@@ -15,25 +15,25 @@ def parse_MPLS_file(br, ip, debug = False, errors = "strict"):
     # Open file ...
     with open(os.path.join(br, "BDMV/PLAYLIST/{:05d}.mpls".format(ip)), "rb") as fobj:
         # Load header ...
-        info["header"] = load_header(fobj, debug = debug, errors = errors)
+        info["header"] = load_header(fobj, debug = debug, errors = errors, indent = indent)
 
         # Load AppInfoPlayList section ...
-        info["AppInfoPlayList"] = load_AppInfoPlayList(fobj, debug = debug, errors = errors)
+        info["AppInfoPlayList"] = load_AppInfoPlayList(fobj, debug = debug, errors = errors, indent = indent)
 
         # Load PlayList section ...
         if info["header"]["PlayListStartAddress"] != 0:
             fobj.seek(info["header"]["PlayListStartAddress"])
-            info["PlayList"] = load_PlayList(fobj, debug = debug, errors = errors)
+            info["PlayList"] = load_PlayList(fobj, debug = debug, errors = errors, indent = indent)
 
         # Load PlayListMark section ...
         if info["header"]["PlayListMarkStartAddress"] != 0:
             fobj.seek(info["header"]["PlayListMarkStartAddress"])
-            info["PlayListMark"] = load_PlayListMark(fobj, debug = debug, errors = errors)
+            info["PlayListMark"] = load_PlayListMark(fobj, debug = debug, errors = errors, indent = indent)
 
         # Load ExtensionData section ...
         if info["header"]["ExtensionDataStartAddress"] != 0:
             fobj.seek(info["header"]["ExtensionDataStartAddress"])
-            info["ExtensionData"] = load_ExtensionData(fobj, debug = debug, errors = errors)
+            info["ExtensionData"] = load_ExtensionData(fobj, debug = debug, errors = errors, indent = indent)
 
     # Return answer ...
     return info
