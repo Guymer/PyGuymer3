8d894bc9c3e7fdaf13282e5ffa8e2abee56dc820
Thomas Guymer <t.m.guymer@thomasguymer.co.uk>
2021-05-15 10:25:17 +0100

    major clean

--- docs/conf.py
+++ docs/conf.py
@@ -1,63 +1,33 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
-# See https://samnicholls.net/2016/06/15/how-to-sphinx-readthedocs/
-# See https://stackoverflow.com/a/62613202
-
-# -- Path setup --------------------------------------------------------------
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-
+# Import standard modules ...
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..'))
 
-# import sphinx_rtd_theme
-# html_theme = 'sphinx_rtd_theme'
-# html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+# Import special modules ...
+import sphinx_rtd_theme
 
-# -- Project information -----------------------------------------------------
+# Expand $PYTHONPATH so that PyGuymer3 can be imported later ...
+sys.path.insert(0, os.path.abspath(".."))
 
-project = 'PyGuymer3'
-copyright = '2021, Thomas Guymer'
-author = 'Thomas Guymer'
+# Set project information ...
+project = "PyGuymer3"
+copyright = "2021, Thomas Guymer"
+author = "Thomas Guymer"
 
-
-# -- General configuration ---------------------------------------------------
-
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
+# Add Sphinx extension modules ...
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.napoleon',
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.napoleon",
 ]
+
+# Configure Sphinx extension modules ...
 autosummary_generate = True
 napoleon_google_docstring = False
 
-# Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+# Set the HTML theme ...
+html_theme = "sphinx_rtd_theme"
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
-
-# -- Options for HTML output -------------------------------------------------
-
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
-html_theme = 'alabaster'
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+# directories to ignore when looking for source files ...
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
