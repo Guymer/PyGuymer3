7268970d9aad40587f2abdf3d5b30f9c85cba8d3
Thomas Guymer <t.m.guymer@thomasguymer.co.uk>
2021-05-15 10:19:31 +0100

    attempt to find the module on RTD

--- docs/conf.py
+++ docs/conf.py
@@ -15,8 +15,11 @@
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../..'))
+sys.path.insert(0, os.path.abspath('..'))
 
+# import sphinx_rtd_theme
+# html_theme = 'sphinx_rtd_theme'
+# html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # -- Project information -----------------------------------------------------
 
