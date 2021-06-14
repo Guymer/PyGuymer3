7d90d59b298005bb6796e840b757d790603a46f4
Thomas Guymer <t.m.guymer@thomasguymer.co.uk>
2021-06-14 20:12:00 +0100

    use new function to fix up rings

--- pyguymer3/geo/buffer_point.py
+++ pyguymer3/geo/buffer_point.py
@@ -27,21 +27,11 @@ def buffer_point(lon1, lat1, dist, kwArgCheck = None, nang = 19, simp = 0.1, deb
     """
 
     # Import standard modules ...
-    import copy
     import math
-
-    # Import special modules ...
-    try:
-        import shapely
-        import shapely.geometry
-        import shapely.ops
-        import shapely.validation
-    except:
-        raise Exception("\"shapely\" is not installed; run \"pip install --user Shapely\"") from None
 
     # Load sub-functions ...
     from .buffer_point_crudely import buffer_point_crudely
-    from ..interpolate import interpolate
+    from .fix_ring import fix_ring
 
     # Check keyword arguments ...
     if kwArgCheck is not None:
@@ -64,140 +54,16 @@ def buffer_point(lon1, lat1, dist, kwArgCheck = None, nang = 19, simp = 0.1, deb
             print("DEBUG: finding the ring using Python")
         ring = buffer_point_crudely(lon1, lat1, dist, nang)
 
-    # Initialise flag, create empty lists and append first point ...
-    flag = True
-    part1 = []
-    part2 = []
-    part1.append((ring[0, 0], ring[0, 1]))
+    # Convert the NumPy array to a [Multi]Polygon, whilst also fixing parts of
+    # it that might have crossed the poles/equator/anti-meridian ...
+    buff = fix_ring(ring)
 
-    # Loop over angles ...
-    # NOTE: Start at 1 (not 0) because the first one will be checked when the
-    #       last one is (as they are the same).
-    for iang in range(1, nang):
-        # Check if the anti-meridian has been crossed between this and the last
-        # point ...
-        if min(ring[iang - 1, 0], ring[iang, 0]) < -90.0 and max(ring[iang - 1, 0], ring[iang, 0]) > +90.0:
-            # NOTE: There is no way with, at most, ~45 degree jumps and only, at
-            #       most, half the Earth's circumference travelled that two
-            #       neighbouring points can straddle *both* -90 longitude and
-            #       +90 longitude.
-            # NOTE: The negative one is East of the anti-meridian.
-            # NOTE: The positive one is West of the anti-meridian.
-
-            # Create short-hand variables for interpolation and then interpolate ...
-            if ring[iang - 1, 0] > ring[iang, 0]:
-                x1 = ring[iang - 1, 0]
-                x2 = +180.0
-                x3 = ring[iang, 0] + 360.0
-                y1 = ring[iang - 1, 1]
-                y3 = ring[iang, 1]
-            else:
-                x1 = ring[iang - 1, 0]
-                x2 = -180.0
-                x3 = ring[iang, 0] - 360.0
-                y1 = ring[iang, 1]
-                y3 = ring[iang - 1, 1]
-            y2 = interpolate(x1, x3, y1, y3, x2)
-
-            # Decide which list needs appending ...
-            if flag:
-                # Append interpolated points on the anti-meridian ...
-                part1.append(( x2, y2))
-                flag = not flag
-                part2.append((-x2, y2))
-            else:
-                # Append interpolated points on the anti-meridian ...
-                part2.append(( x2, y2))
-                flag = not flag
-                part1.append((-x2, y2))
-
-        # Decide which list needs appending and append point ...
-        if flag:
-            part1.append((ring[iang, 0], ring[iang, 1]))
-        else:
-            part2.append((ring[iang, 0], ring[iang, 1]))
-
-    # Check flag ...
-    # NOTE: The only way a continuous ring can cross the anti-meridian an odd
-    #       number of times is if it also crosses one of the poles.
-    if not flag:
-        # Check if it is in the northern hemi-sphere ...
-        north = True
-        for coord in part1:
-            if coord[1] < 0.0:
-                north = False
-                break
-        for coord in part2:
-            if coord[1] < 0.0:
-                north = False
-                break
-
-        # Check if it is in the southern hemi-sphere ...
-        south = True
-        for coord in part1:
-            if coord[1] > 0.0:
-                south = False
-                break
-        for coord in part2:
-            if coord[1] > 0.0:
-                south = False
-                break
-
-        # Check hemi-spheres ...
-        if not north and not south:
-            raise Exception("the ring crossed the anti-meridian an odd number of times and exists in both hemi-spheres") from None
-
-        # Add vertical lines to the pole at both ends and over-write lists ...
-        tmp = []
-        tmp += part1
-        if north:
-            tmp += [(part1[-1][0], +90.0), (part2[0][0], +90.0)]
-        if south:
-            tmp += [(part1[-1][0], -90.0), (part2[0][0], -90.0)]
-        tmp += part2
-        part1 = copy.copy(tmp)
-        part2 = []
-
-    # HACK: Sometimes the iteration returns not exactly identical locations for
-    #       0 degrees and 360 degrees (call it rounding error; if only shapely
-    #       had a "tolerance" parameter).
-    if part1[0] != part1[-1]:
-        part1[-1] = part1[0]
-    if len(part2) > 0:
-        if part2[0] != part2[-1]:
-            part2[-1] = part2[0]
-
-    # Create a list of Polygons if there are enough unique points to make a
-    # Polygon ...
-    buff = []
-    if len(set(part1)) >= 3:
-        tmp = shapely.geometry.polygon.Polygon(part1)
-        if not tmp.is_valid:
-            if debug:
-                print("DEBUG: \"tmp\" is not a valid Polygon ({0:s})".format(shapely.validation.explain_validity(tmp)))
-        else:
-            buff.append(tmp)
-    if len(set(part2)) >= 3:
-        tmp = shapely.geometry.polygon.Polygon(part2)
-        if not tmp.is_valid:
-            if debug:
-                print("DEBUG: \"tmp\" is not a valid Polygon ({0:s})".format(shapely.validation.explain_validity(tmp)))
-        else:
-            buff.append(tmp)
-
-    # Convert list of Polygons to (unified) MultiPolygon ...
-    buff = shapely.ops.unary_union(buff)
-
-    # Check MultiPolygon ...
-    if not buff.is_valid:
-        raise Exception("\"buff\" is not a valid [Multi]Polygon ({0:s})".format(shapely.validation.explain_validity(buff))) from None
-
-    # Check if the user wants to simplify the MultiPolygon ...
+    # Check if the user wants to simplify the [Multi]Polygon ...
     if simp > 0.0:
-        # Simplify MultiPolygon ...
+        # Simplify [Multi]Polygon ...
         buffSimp = buff.simplify(simp)
 
-        # Check simplified MultiPolygon ...
+        # Check simplified [Multi]Polygon ...
         if buffSimp.is_valid:
             # Return simplified answer ...
             return buffSimp
