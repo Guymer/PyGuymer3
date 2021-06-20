d0f89653957cfeee64acadf6adb4c6bc21ffb89e
Thomas Guymer <t.m.guymer@thomasguymer.co.uk>
2021-06-20 12:01:58 +0100

    simplified comparisons

--- pyguymer3/convert_pretty_bytes_to_bytes.py
+++ pyguymer3/convert_pretty_bytes_to_bytes.py
@@ -7,15 +7,15 @@ def convert_pretty_bytes_to_bytes(string):
     units = re.sub(r"[0-9\.]", "", string).upper()
 
     # Scale value ...
-    if units == "KB" or units == "KiB":
+    if units in ["KB", "KiB"]:
         val *= 1024.0                                                           # [B]
-    elif units == "MB" or units == "MiB":
+    elif units in ["MB", "MiB"]:
         val *= 1024.0 * 1024.0                                                  # [B]
-    elif units == "GB" or units == "GiB":
+    elif units in ["GB", "GiB"]:
         val *= 1024.0 * 1024.0 * 1024.0                                         # [B]
-    elif units == "TB" or units == "TiB":
+    elif units in ["TB", "TiB"]:
         val *= 1024.0 * 1024.0 * 1024.0 * 1024.0                                # [B]
-    elif units == "PB" or units == "PiB":
+    elif units in ["PB", "PiB"]:
         val *= 1024.0 * 1024.0 * 1024.0 * 1024.0 * 1024.0                       # [B]
 
     # Return answer ...
