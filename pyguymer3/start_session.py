909f533471d6cb1b23695bf2bdb69b784f18bea9
Thomas Guymer <t.m.guymer@thomasguymer.co.uk>
2021-12-30 07:42:54 +0000

    use a single dictionary to define the header

--- pyguymer3/start_session.py
+++ pyguymer3/start_session.py
@@ -8,12 +8,16 @@ def start_session():
     # Start session ...
     sess = requests.Session()
     sess.allow_redirects = True
-    sess.headers.update({"Accept" : "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"})
-    sess.headers.update({"Accept-Language" : "en-GB,en;q=0.5"})
-    sess.headers.update({"Accept-Encoding" : "gzip, deflate"})
-    sess.headers.update({"DNT" : "1"})
-    sess.headers.update({"Upgrade-Insecure-Requests" : "1"})
-    sess.headers.update({"User-Agent" : "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:68.0) Gecko/20100101 Firefox/68.0"})
+    sess.headers.update(
+        {
+                               "Accept" : "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+                      "Accept-Language" : "en-GB,en;q=0.5",
+                      "Accept-Encoding" : "gzip, deflate",
+                                  "DNT" : "1",
+            "Upgrade-Insecure-Requests" : "1",
+                           "User-Agent" : "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:68.0) Gecko/20100101 Firefox/68.0",
+        }
+    )
     sess.max_redirects = 5
 
     # Return answer ...
