2c1c3ea527dd01ea212a25c59d2f5dafddb97adb
Thomas Guymer <t.m.guymer@thomasguymer.co.uk>
2022-01-23 15:44:16 +0000

    major re-write

--- pyguymer3/remove_almost_empty_directories.py
+++ pyguymer3/remove_almost_empty_directories.py
@@ -1,21 +1,60 @@
-def remove_almost_empty_directories(path):
+def remove_almost_empty_directories(path, kwArgCheck = None, debug = True, ignorableFiles = (".directory", ".DS_Store", "._.DS_Store", "Thumbs.db"), remove = False):
+    """Remove directories which are almost empty.
+
+    Parameters
+    ----------
+    path : str
+        the directory to search
+    debug : bool, optional
+        print debug messages, default True
+    ignorableFiles : tuple of str, optional
+        the tuple of files which can safely be ignored, default (".directory", ".DS_Store", "._.DS_Store", "Thumbs.db")
+    remove : bool, optional
+        remove almost empty directories, default False
+    """
+
     # Import standard modules ...
     import os
 
-    # Set counter ...
+    # Check keyword arguments ...
+    if kwArgCheck is not None:
+        print(f"WARNING: \"{__name__}\" has been called with an extra positional argument")
+
+    # Initialize counter ...
     i = 0
 
     # Loop over all the contents of the passed directory ...
-    for root, dnames, fnames in os.walk(path, topdown = False):
-        # Loop over all sub-directories ...
-        for dname in dnames:
-            # List its contents and check if the only content is a ".directory"
-            # file ...
-            if os.listdir(os.path.join(root, dname)) == [".directory"]:
-                # Increment counter and remove directory ...
-                i += 1
-                os.remove(os.path.join(root, dname, ".directory"))
-                os.rmdir(os.path.join(root, dname))
+    for root, dnames, fnames in os.walk(path):
+        # Skip this directory if there are sub-directories ...
+        if len(dnames):
+            continue
+
+        # Make a list of all of the non-ignorable files in this directory ...
+        nonIgnorableFiles = fnames.copy()
+        for ignorableFile in ignorableFiles:
+            if ignorableFile in nonIgnorableFiles:
+                nonIgnorableFiles.remove(ignorableFile)
+
+        # Skip this directory if there are non-ignorable files ...
+        if len(nonIgnorableFiles):
+            continue
+
+        # Increment counter ...
+        i += 1
+
+        # Remove all ignorable files ...
+        for ignorableFile in ignorableFiles:
+            if ignorableFile in fnames:
+                if debug:
+                    print(f"Removing \"{root}/{ignorableFile}\" ...")
+                if remove:
+                    os.remove(f"{root}/{ignorableFile}")
+
+        # Remove directory ...
+        if debug:
+            print(f"Removing \"{root}\" ...")
+        if remove:
+            os.rmdir(root)
 
     # Return counter ...
     return i
