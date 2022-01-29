d8738fd29938fda1d6b025b5c701ecc463077d5a
Thomas Guymer <t.m.guymer@thomasguymer.co.uk>
2022-01-29 14:01:48 +0000

    added function to add lakes

--- pyguymer3/geo/__init__.py
+++ pyguymer3/geo/__init__.py
@@ -22,6 +22,7 @@ itself once)
 from ._add_bathymetry import _add_bathymetry
 from ._add_glaciatedAreas import _add_glaciatedAreas
 from ._add_land import _add_land
+from ._add_lakes import _add_lakes
 from ._add_minorIslands import _add_minorIslands
 from ._add_reefs import _add_reefs
 from ._buffer_points_crudely import _buffer_points_crudely
--- pyguymer3/geo/add_map_underlay.py
+++ pyguymer3/geo/add_map_underlay.py
@@ -3,13 +3,13 @@ def add_map_underlay(axis, kwArgCheck = None, cultural = True, debug = False, ph
     #         * https://www.naturalearthdata.com/downloads/10m-cultural-vectors/
     # TODO: Physical datasets:
     #         * https://www.naturalearthdata.com/downloads/10m-physical-vectors/10m-rivers-lake-centerlines/
-    #         * https://www.naturalearthdata.com/downloads/10m-physical-vectors/10m-lakes/
     #         * https://www.naturalearthdata.com/downloads/10m-physical-vectors/10m-playas/
     #         * https://www.naturalearthdata.com/downloads/10m-physical-vectors/10m-antarctic-ice-shelves/
 
     # Import sub-functions ...
     from ._add_bathymetry import _add_bathymetry
     from ._add_glaciatedAreas import _add_glaciatedAreas
+    from ._add_lakes import _add_lakes
     from ._add_land import _add_land
     from ._add_minorIslands import _add_minorIslands
     from ._add_reefs import _add_reefs
@@ -22,13 +22,18 @@ def add_map_underlay(axis, kwArgCheck = None, cultural = True, debug = False, ph
 
     # Add physical datasets ...
     if physical:
-        # Oceans ...
+        # Water ...
         _add_bathymetry(axis, debug = debug, resolution = resolution)
+
+        # Water overlays ...
         _add_reefs(axis, debug = debug, resolution = resolution)
 
         # Land ...
         _add_land(axis, debug = debug, resolution = resolution)
         _add_minorIslands(axis, debug = debug, resolution = resolution)
+
+        # Land overlays ...
+        _add_lakes(axis, debug = debug, resolution = resolution)
         _add_glaciatedAreas(axis, debug = debug, resolution = resolution)
 
     # Add cultural datasets ...
