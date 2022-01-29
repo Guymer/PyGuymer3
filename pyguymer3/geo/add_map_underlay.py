5d09e0b40fb57666f95cab749dade754acf4bce6
Thomas Guymer <t.m.guymer@thomasguymer.co.uk>
2022-01-29 13:49:15 +0000

    added to-do lists

--- pyguymer3/geo/add_map_underlay.py
+++ pyguymer3/geo/add_map_underlay.py
@@ -1,4 +1,13 @@
-def add_map_underlay(axis, kwArgCheck = None, debug = False, resolution = "10m"):
+def add_map_underlay(axis, kwArgCheck = None, cultural = True, debug = False, physical = True, resolution = "10m"):
+    # TODO: Cultural datasets:
+    #         * https://www.naturalearthdata.com/downloads/10m-cultural-vectors/
+    # TODO: Physical datasets:
+    #         * https://www.naturalearthdata.com/downloads/10m-physical-vectors/10m-rivers-lake-centerlines/
+    #         * https://www.naturalearthdata.com/downloads/10m-physical-vectors/10m-lakes/
+    #         * https://www.naturalearthdata.com/downloads/10m-physical-vectors/10m-playas/
+    #         * https://www.naturalearthdata.com/downloads/10m-physical-vectors/10m-antarctic-ice-shelves/
+    #         * https://www.naturalearthdata.com/downloads/10m-physical-vectors/10m-glaciated-areas/
+
     # Import sub-functions ...
     from ._add_bathymetry import _add_bathymetry
     from ._add_land import _add_land
@@ -11,8 +20,13 @@ def add_map_underlay(axis, kwArgCheck = None, debug = False, resolution = "10m")
 
     # **************************************************************************
 
-    # Add datasets ...
-    _add_bathymetry(axis, debug = debug, resolution = resolution)
-    _add_land(axis, debug = debug, resolution = resolution)
-    _add_minorIslands(axis, debug = debug, resolution = resolution)
-    _add_reefs(axis, debug = debug, resolution = resolution)
+    # Add physical datasets ...
+    if physical:
+        _add_bathymetry(axis, debug = debug, resolution = resolution)
+        _add_land(axis, debug = debug, resolution = resolution)
+        _add_minorIslands(axis, debug = debug, resolution = resolution)
+        _add_reefs(axis, debug = debug, resolution = resolution)
+
+    # Add cultural datasets ...
+    if cultural:
+        True
