35fb4cd3c69eb26259238bef60dffa2e8cf85d41
Thomas Guymer <t.m.guymer@thomasguymer.co.uk>
2022-01-30 11:18:11 +0000

    add background a lot simpler (this also makes it work for non-global plots)

--- pyguymer3/geo/_add_background.py
+++ pyguymer3/geo/_add_background.py
@@ -4,22 +4,9 @@ def _add_background(axis, kwArgCheck = None, debug = False):
 
     # Import special modules ...
     try:
-        import cartopy
-    except:
-        raise Exception("\"cartopy\" is not installed; run \"pip install --user Cartopy\"") from None
-    try:
         import matplotlib
     except:
         raise Exception("\"matplotlib\" is not installed; run \"pip install --user matplotlib\"") from None
-    try:
-        import shapely
-        import shapely.geometry
-        import shapely.validation
-    except:
-        raise Exception("\"shapely\" is not installed; run \"pip install --user Shapely\"") from None
-
-    # Import sub-functions ...
-    from ._debug import _debug
 
     # Check keyword arguments ...
     if kwArgCheck is not None:
@@ -32,28 +19,5 @@ def _add_background(axis, kwArgCheck = None, debug = False):
     if debug:
         print(f"INFO: \"background\" is ({facecolor[0]:.6f},{facecolor[1]:.6f},{facecolor[2]:.6f},{facecolor[3]:.6f}).")
 
-    # Define Earth (as a correctly oriented Polygon) ...
-    earth = shapely.geometry.polygon.Polygon(
-        [
-            (-180.0,  +90.0),
-            (-180.0,  -90.0),
-            (+180.0,  -90.0),
-            (+180.0,  +90.0),
-            (-180.0,  +90.0),
-        ]
-    )
-    if not isinstance(earth, shapely.geometry.polygon.Polygon):
-        raise Exception("\"earth\" is not a Polygon") from None
-    if not earth.is_valid:
-        _debug(earth)
-        raise Exception(f"\"earth\" is not a valid Polygon ({shapely.validation.explain_validity(earth)})") from None
-    if earth.is_empty:
-        raise Exception("\"earth\" is an empty Polygon") from None
-
-    # Plot geometry ...
-    axis.add_geometries(
-        [earth],
-        cartopy.crs.PlateCarree(),
-        edgecolor = "none",
-        facecolor = facecolor,
-    )
+    # Set background ...
+    axis.set_facecolor(facecolor)
