9a74062ed468e0e35d392bad13824de0155bb00e
Thomas Guymer <t.m.guymer@thomasguymer.co.uk>
2022-07-20 08:39:11 +0100

    remove fixup part

--- pyguymer3/geo/fillinSrc/fillin_LinearRing.py
+++ pyguymer3/geo/fillinSrc/fillin_LinearRing.py
@@ -1,8 +1,8 @@
-def fillin_LinearRing(ring, fill, kwArgCheck = None, debug = False, fillSpace = "EuclideanSpace", tol = 1.0e-10):
+def fillin_LinearRing(ring, fill, kwArgCheck = None, debug = False, fillSpace = "EuclideanSpace"):
     """Fill in a LinearRing
 
     This function reads in a LinearRing that exists on the surface of the Earth
-    and returns a LinearRing of the same LinearRing filled in by a constant
+    and returns a LineString of the same LinearRing filled in by a constant
     distance: either in degrees in Euclidean space; or in metres in Geodesic
     space.
 
@@ -16,8 +16,6 @@ def fillin_LinearRing(ring, fill, kwArgCheck = None, debug = False, fillSpace = 
             print debug messages
     fillSpace : str, optional
             the geometric space to perform the filling in (either "EuclideanSpace" or "GeodesicSpace")
-    tol : float, optional
-            the Euclidean distance that defines two points as being the same (in degrees)
 
     Returns
     -------
@@ -27,10 +25,6 @@ def fillin_LinearRing(ring, fill, kwArgCheck = None, debug = False, fillSpace = 
 
     # Import special modules ...
     try:
-        import numpy
-    except:
-        raise Exception("\"numpy\" is not installed; run \"pip install --user numpy\"") from None
-    try:
         import shapely
         import shapely.geometry
         import shapely.validation
@@ -38,7 +32,7 @@ def fillin_LinearRing(ring, fill, kwArgCheck = None, debug = False, fillSpace = 
         raise Exception("\"shapely\" is not installed; run \"pip install --user Shapely\"") from None
 
     # Import sub-functions ...
-    from ._debug import _debug
+    from .._debug import _debug
     from .fillin_CoordinateSequence import fillin_CoordinateSequence
 
     # Check keyword arguments ...
@@ -54,70 +48,5 @@ def fillin_LinearRing(ring, fill, kwArgCheck = None, debug = False, fillSpace = 
     if ring.is_empty:
         raise Exception("\"ring\" is an empty LinearRing") from None
 
-    # **************************************************************************
-
-    # Convert the CoordinateSequence to a NumPy array ...
-    points1 = numpy.array(ring.coords)                                          # [°]
-
-    # Create short-hand ...
-    npoint = points1.shape[0]
-
-    # Initialize list ...
-    points2 = []                                                                # [°]
-    points2.append(points1[0, :])                                               # [°]
-
-    # Initialize flag ...
-    skip = False
-
-    # Loop over points ...
-    for ipoint in range(1, npoint - 1):
-        # Skip this point if it needs skipping ...
-        if skip:
-            # Re-set flag ...
-            skip = False
-
-            continue
-
-        # Find the Euclidean distance between the points either side of this
-        # point ...
-        dr = numpy.hypot(points1[ipoint - 1, 0] - points1[ipoint + 1, 0], points1[ipoint - 1, 1] - points1[ipoint + 1, 1])  # [°]
-
-        # Skip this point (and the next one) if it is a bad spike ...
-        if dr < tol:
-            if debug:
-                print(f"INFO: Removing spike between ({points1[ipoint, 0]:.6f}°,{points1[ipoint, 1]:.6f}°) and ({points1[ipoint + 1, 0]:.6f}°,{points1[ipoint + 1, 1]:.6f}°).")
-
-            # Set flag ...
-            skip = True
-
-            continue
-
-        # Append point to list ...
-        points2.append(points1[ipoint, :])                                      # [°]
-
-    # Append last point to list (if it shouldn't be skipped) ...
-    if not skip:
-        points2.append(points1[-1, :])                                          # [°]
-
-    # Clean up ...
-    del points1
-
-    # Convert list of points in to a LinearRing ...
-    coords = shapely.geometry.polygon.LinearRing(points2)
-
-    # Clean up ...
-    del points2
-
-    # Check LinearRing ...
-    if not isinstance(coords, shapely.geometry.polygon.LinearRing):
-        raise TypeError("\"coords\" is not a LinearRing") from None
-    if not coords.is_valid:
-        _debug(coords)
-        raise Exception(f"\"coords\" is not a valid LinearRing ({shapely.validation.explain_validity(coords)})") from None
-    if coords.is_empty:
-        raise Exception("\"coords\" is an empty LinearRing") from None
-
-    # **************************************************************************
-
     # Return filled in LinearRing ...
-    return fillin_CoordinateSequence(coords.coords, fill, debug = debug, fillSpace = fillSpace)
+    return fillin_CoordinateSequence(ring.coords, fill, debug = debug, fillSpace = fillSpace)
