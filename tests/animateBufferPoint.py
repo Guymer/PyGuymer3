a91ee3bebe0e01b131f4088dbb74a8cf49baa018
Thomas Guymer <t.m.guymer@thomasguymer.co.uk>
2022-07-23 11:51:00 +0100

    add a script to show what map projections truly look like

---
+++ tests/animateBufferPoint.py
@@ -0,0 +1,168 @@
+#!/usr/bin/env python3
+
+# Use the proper idiom in the main module ...
+# NOTE: See https://docs.python.org/3.9/library/multiprocessing.html#multiprocessing-programming
+if __name__ == "__main__":
+    # Import standard modules ...
+    import os
+
+    # Import special modules ...
+    try:
+        import cartopy
+    except:
+        raise Exception("\"cartopy\" is not installed; run \"pip install --user Cartopy\"") from None
+    try:
+        import geojson
+    except:
+        raise Exception("\"geojson\" is not installed; run \"pip install --user geojson\"") from None
+    try:
+        import matplotlib
+        matplotlib.use("Agg")                                                   # NOTE: See https://matplotlib.org/stable/gallery/user_interfaces/canvasagg.html
+        import matplotlib.pyplot
+    except:
+        raise Exception("\"matplotlib\" is not installed; run \"pip install --user matplotlib\"") from None
+    try:
+        import numpy
+    except:
+        raise Exception("\"numpy\" is not installed; run \"pip install --user numpy\"") from None
+    try:
+        import PIL
+        import PIL.Image
+    except:
+        raise Exception("\"PIL\" is not installed; run \"pip install --user Pillow\"") from None
+    try:
+        import shapely
+        import shapely.geometry
+    except:
+        raise Exception("\"shapely\" is not installed; run \"pip install --user Shapely\"") from None
+
+    # Import my modules ...
+    try:
+        import pyguymer3
+        import pyguymer3.geo
+        import pyguymer3.image
+    except:
+        raise Exception("\"pyguymer3\" is not installed; you need to have the Python module from https://github.com/Guymer/PyGuymer3 located somewhere in your $PYTHONPATH") from None
+
+    # Configure PIL to open images up to 1 GiP ...
+    PIL.Image.MAX_IMAGE_PIXELS = 1024 * 1024 * 1024                             # [px]
+
+    print(f"Testing \"{pyguymer3.__path__[0]}\" ...")
+
+    # Configure functions ...
+    debug = False
+    dist = 2.0e6                                                                # [m]
+    fill = 1.0                                                                  # [°]
+    fillSpace = "EuclideanSpace"
+    nang = 361                                                                  # [#]
+    simp = -1.0                                                                 # [°]
+    tol = 1.0e-10                                                               # [°]
+
+    # Make output directory ...
+    if not os.path.exists("animateBufferPoint"):
+        os.mkdir("animateBufferPoint")
+
+    # **************************************************************************
+
+    # Loop over latitude ...
+    for lat in range(-90, +90 + 20, 20):
+        # Loop over longitude ...
+        for lon in range(-180, +180 + 20, 20):
+            # Determine file names ...
+            fname = f"animateBufferPoint/lon={lon:+04d},lat={lat:+03d}.png"
+            jname = f"animateBufferPoint/lon={lon:+04d},lat={lat:+03d}.geojson"
+
+            # Skip if both outputs already exist ...
+            if os.path.exists(fname) and os.path.exists(jname):
+                continue
+
+            print(f" > Making \"{jname}\" and \"{fname}\" ...")
+
+            # Create figure ...
+            fg = matplotlib.pyplot.figure(figsize = (6, 6), dpi = 150)
+
+            # Create first subplot ...
+            ax1 = fg.add_subplot(2, 2, 1, projection = cartopy.crs.Robinson())
+            ax1.set_global()
+            pyguymer3.geo.add_map_background(ax1)
+            pyguymer3.geo.add_horizontal_gridlines(ax1, [-180.0, +180.0, -90.0, +90.0], locs = range(-90, 135, 45))
+            pyguymer3.geo.add_vertical_gridlines(ax1, [-180.0, +180.0, -90.0, +90.0], locs = range(-180, 225, 45))
+            ax1.coastlines(resolution = "110m", color = "black", linewidth = 0.1)
+
+            # Create second subplot ...
+            ax2 = fg.add_subplot(2, 2, 2, projection = cartopy.crs.Orthographic(central_longitude = lon, central_latitude = lat))
+            ax2.set_global()
+            pyguymer3.geo.add_map_background(ax2)
+            pyguymer3.geo.add_horizontal_gridlines(ax2, [-180.0, +180.0, -90.0, +90.0], locs = range(-90, 135, 45))
+            pyguymer3.geo.add_vertical_gridlines(ax2, [-180.0, +180.0, -90.0, +90.0], locs = range(-180, 225, 45))
+            ax2.coastlines(resolution = "110m", color = "black", linewidth = 0.1)
+
+            # Create third subplot ...
+            ax3 = fg.add_subplot(2, 2, (3, 4))
+            ax3.grid()
+            ax3.set_aspect("equal")
+            ax3.set_xlabel("Longitude [°]")
+            ax3.set_xlim(-180.0, +180.0)
+            ax3.set_xticks([-180.0, -135.0, -90.0, -45.0, 0.0, +45.0, +90.0, +135.0, +180.0])
+            ax3.set_ylabel("Latitude [°]")
+            ax3.set_ylim(-90.0, +90.0)
+            ax3.set_yticks([-90.0, -45.0, 0.0, +45.0, +90.0])
+
+            # Create point ...
+            point = shapely.geometry.point.Point(lon, lat)
+
+            # Buffer Point and plot it thrice ...
+            buff0 = pyguymer3.geo.buffer(point, dist, debug = debug, fill = fill, fillSpace = fillSpace, nang = nang, simp = simp, tol = tol)
+            ax1.add_geometries([buff0], cartopy.crs.PlateCarree(), edgecolor = (1.0, 0.0, 0.0, 1.0), facecolor = (1.0, 0.0, 0.0, 0.5), linewidth = 1.0)
+            ax2.add_geometries([buff0], cartopy.crs.PlateCarree(), edgecolor = (1.0, 0.0, 0.0, 1.0), facecolor = (1.0, 0.0, 0.0, 0.5), linewidth = 1.0)
+            for poly in pyguymer3.geo.extract_polys(buff0):
+                coords = numpy.array(poly.exterior.coords)
+                ax3.plot(coords[:, 0], coords[:, 1], color = (1.0, 0.0, 0.0, 1.0))
+                del coords
+
+            # Save GeoJSON ...
+            with open(jname, "wt", encoding = "utf-8") as fobj:
+                geojson.dump(
+                    buff0,
+                    fobj,
+                    ensure_ascii = False,
+                          indent = 4,
+                       sort_keys = True,
+                )
+
+            # Clean up ...
+            del buff0
+
+            # Save figure ...
+            fg.suptitle(f"({lon:.1f},{lat:.1f}) buffered by {0.001 * dist:,.0f}km")
+            fg.savefig(fname, bbox_inches = "tight", dpi = 150, pad_inches = 0.1)
+            pyguymer3.image.optimize_image(fname, strip = True)
+            matplotlib.pyplot.close(fg)
+
+    # **************************************************************************
+
+    print(" > Making \"animateBufferPoint.webp\" ...")
+
+    # Initialize list ...
+    images = []
+
+    # Loop over latitude ...
+    for lat in range(-90, +90 + 20, 20):
+        # Loop over longitude ...
+        for lon in range(-180, +180 + 20, 20):
+            # Determine file names ...
+            fname = f"animateBufferPoint/lon={lon:+04d},lat={lat:+03d}.png"
+
+            # Open image as RGB (even if it is paletted) ...
+            image = PIL.Image.open(fname).convert("RGB")
+
+            # Append it to the list ...
+            images.append(image)
+
+    # Save 25fps WEBP ...
+    images[0].save("animateBufferPoint.webp", lossless = True, quality = 100, method = 6, save_all = True, append_images = images[1:], duration = 40, loop = 0, minimize_size = True)
+
+    # Clean up ...
+    for image in images:
+        image.close()
+    del images
