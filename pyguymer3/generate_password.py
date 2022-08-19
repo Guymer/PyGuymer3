937e708e512c652772886455d8d660303a600656
Thomas Guymer <t.m.guymer@thomasguymer.co.uk>
2022-08-19 14:19:07 +0100

    use secrets module instead

--- pyguymer3/generate_password.py
+++ pyguymer3/generate_password.py
@@ -1,26 +1,7 @@
 def generate_password():
     # Import standard modules ...
-    import base64
-    import re
+    import secrets
+    import string
 
-    # Define constant ...
-    test = re.compile(r"[a-zA-Z0-9][a-zA-Z0-9][a-zA-Z0-9][a-zA-Z0-9]")
-
-    # Open stream ...
-    with open("/dev/random", "rb") as devrand:
-        # Clear the password ...
-        passwd = ""
-
-        # Infinite loop ...
-        while True:
-            # Obtain a 4 UTF-8 character stub from 3 random bytes ...
-            stub = base64.b64encode(devrand.read(3)).decode("utf-8")
-
-            # Check that it is just the 4 UTF-8 characters I want and add it to
-            # the password ...
-            if test.match(stub) is not None:
-                passwd += stub
-
-                # Exit if the password is the correct length ...
-                if len(passwd) == 20:
-                    return passwd
+    # Return twenty randomly/securely chosen digits and ASCII letters ...
+    return "".join(secrets.choice(string.digits + string.ascii_letters) for i in range(20))
