9276f7476bee691724f93bd7d0cf81ba60daeb2a
Thomas Guymer <t.m.guymer@thomasguymer.co.uk>
2022-12-25 11:09:33 +0000

    added a function to return the pixel resolution

--- pyguymer3/openstreetmap/__init__.py
+++ pyguymer3/openstreetmap/__init__.py
@@ -5,4 +5,5 @@ A Python sub-module containing functions relating to openstreetmap.org
 # Import sub-functions ...
 from .deg2num import deg2num
 from .num2deg import num2deg
+from .res import res
 from .tile import tile
---
+++ pyguymer3/openstreetmap/res.py
@@ -0,0 +1,21 @@
+def res(lat_deg, zoom):
+    # NOTE: https://wiki.openstreetmap.org/wiki/Slippy_map_tilenames#Resolution_and_Scale
+
+    # Import standard modules ...
+    import math
+
+    # Define contants  ...
+    # NOTE: See https://en.wikipedia.org/wiki/Earth_radius#Mean_radius
+    # NOTE: Using a radius of 6,371,008.8 m equates to:
+    #         * 1 °  = 111.195 km
+    #         * 1 m° = 111.195 m
+    #         * 1 μ° = 11.1195 cm
+    radiusOfEarth = 6371008.8                                                   # [m]
+    circumOfEarth = 2.0 * math.pi * radiusOfEarth                               # [m]
+
+    lat_rad = math.radians(lat_deg)
+    n = 2.0 ** zoom
+    resoluOfEarth = circumOfEarth * math.cos(lat_rad) / (256.0 * n)             # [m/px]
+
+    # Return answer ...
+    return resoluOfEarth
