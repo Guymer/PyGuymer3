7b17919315498676a1c5bc5a5a9f7af0ac83f821
Thomas Guymer <t.m.guymer@thomasguymer.co.uk>
2023-01-22 09:12:50 +0000

    update headers

--- pyguymer3/start_session.py
+++ pyguymer3/start_session.py
@@ -14,11 +14,11 @@ def start_session():
     sess.headers.update(
         {
                                "Accept" : "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-                      "Accept-Encoding" : "gzip, deflate",
+                      "Accept-Encoding" : "gzip, deflate, br",
                       "Accept-Language" : "en-GB,en;q=0.9",
                                   "DNT" : "1",
             "Upgrade-Insecure-Requests" : "1",
-                           "User-Agent" : "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.2 Safari/605.1.15",
+                           "User-Agent" : "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.2 Safari/605.1.15",
         }
     )
     sess.max_redirects = 5
