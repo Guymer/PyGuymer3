3969fcee6325c2116c6ccb8dc136121b03891666
Thomas Guymer <t.m.guymer@thomasguymer.co.uk>
2023-02-03 08:53:51 +0000

    tidy comments

--- pyguymer3/geo/clipLatitude.py
+++ pyguymer3/geo/clipLatitude.py
@@ -2,4 +2,5 @@
 
 # Define function ...
 def clipLatitude(lat):
-    return max(-90.0, min(+90.0, lat)) # -90° ≤ lat ≤ +90°
+    # -90° ≤ lat ≤ +90° ...
+    return max(-90.0, min(+90.0, lat))
--- pyguymer3/geo/wrapLongitude.py
+++ pyguymer3/geo/wrapLongitude.py
@@ -2,4 +2,6 @@
 
 # Define function ...
 def wrapLongitude(lon):
-    return ((lon + 180.0) % 360.0) - 180.0 # -180° ≤ lon < +180° (i.e., +180° is not accepted, it is wrapped around to -180°)
+    # -180° ≤ lon < +180° (i.e., +180° is not accepted, it is wrapped around to
+    # -180°) ...
+    return ((lon + 180.0) % 360.0) - 180.0
