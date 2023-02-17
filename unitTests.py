46f0eed6f7aef3c9d8f1832140bef6f5d2a997ed
Thomas Guymer <t.m.guymer@thomasguymer.co.uk>
2023-02-17 11:03:44 +0000

    add a first stab at using the "unittest" standard module

---
+++ unitTests.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+
+# Import standard modules ...
+import unittest
+
+# Import my modules ...
+try:
+    import pyguymer3
+except:
+    raise Exception("\"pyguymer3\" is not installed; you need to have the Python module from https://github.com/Guymer/PyGuymer3 located somewhere in your $PYTHONPATH") from None
+
+# Define a test case ...
+class MyTestCase(unittest.TestCase):
+    # Define a test ...
+    def test_convertBytesToPrettyBytes(self):
+        self.assertSequenceEqual(
+            pyguymer3.convert_bytes_to_pretty_bytes(16.0 * 1024.0 * 1024.0),
+            (16.0, "MiB"),
+        )
+
+    # Define a test ...
+    def test_convertPrettyBytesToBytes(self):
+        self.assertEqual(
+            pyguymer3.convert_pretty_bytes_to_bytes("16.0 MiB"),
+            16.0 * 1024.0 * 1024.0,
+        )
+
+    # Define a test ...
+    def test_findIntegerDivisors(self):
+        self.assertSequenceEqual(
+            pyguymer3.find_integer_divisors(12),
+            [2, 3, 4, 6],
+        )
+
+    # Define a test ...
+    def test_interpolate(self):
+        self.assertEqual(
+            pyguymer3.interpolate(1.0, 3.0, 2.0, 4.0, 2.0),
+            3.0,
+        )
+
+    # Define a test ...
+    def test_intersection(self):
+        self.assertSequenceEqual(
+            pyguymer3.intersection(
+                (1.0, 3.0),
+                (3.0, 1.0),
+                (1.0, 1.0),
+                (3.0, 3.0),
+            ).tolist(),
+            [2.0, 2.0],
+        )
+
+    # Define a test ...
+    def test_makePathSafe(self):
+        self.assertEqual(
+            pyguymer3.make_path_safe(".what do you think of this path?"),
+            " .what do you think of this path",
+        )
+
+# Use the proper idiom in the main module ...
+# NOTE: See https://docs.python.org/3.10/library/multiprocessing.html#the-spawn-and-forkserver-start-methods
+if __name__ == "__main__":
+    # Run the tests ...
+    unittest.main()
