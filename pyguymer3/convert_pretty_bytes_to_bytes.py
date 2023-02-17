7cde60bbb7b8d24cf5f5e6922f40a2bc5136afc2
Thomas Guymer <t.m.guymer@thomasguymer.co.uk>
2023-02-17 11:01:43 +0000

    bugfix: correctly handle spaces; correct handle mixed case characters

--- pyguymer3/convert_pretty_bytes_to_bytes.py
+++ pyguymer3/convert_pretty_bytes_to_bytes.py
@@ -6,8 +6,8 @@ def convert_pretty_bytes_to_bytes(string, /):
     import re
 
     # Extract digits (with decimal point) and letters separately ...
-    val = float(re.sub(r"[A-Z]", "", string.upper()))                           # [?]
-    units = re.sub(r"[0-9\.]", "", string).upper()
+    val = float(re.sub(r"[a-zA-Z]", "", string).strip())                        # [?]
+    units = re.sub(r"[0-9\.]", "", string).strip()
 
     # Scale value ...
     if units in ["KB", "KiB"]:
