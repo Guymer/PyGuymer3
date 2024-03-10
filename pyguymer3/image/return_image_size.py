54fd60fb2bc2ffb5aa88e7cb2757d60b64c78d3b
Thomas Guymer <t.m.guymer@thomasguymer.co.uk>
2024-03-10 11:34:14 +0000

    add ability to open compressed images

--- pyguymer3/image/return_image_size.py
+++ pyguymer3/image/return_image_size.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
 # Define function ...
-def return_image_size(fname, /):
+def return_image_size(fname, /, *, compressed = False):
+    # Import standard modules ...
+    import gzip
+
     # Import special modules ...
     try:
         import PIL
@@ -10,9 +13,17 @@ def return_image_size(fname, /):
     except:
         raise Exception("\"PIL\" is not installed; run \"pip install --user Pillow\"") from None
 
-    # Open image as RGB (even if it is paletted) ...
-    with PIL.Image.open(fname) as iObj:
-        im = iObj.convert("RGB")
+    # Check if the image is compressed ...
+    if compressed:
+        # Open compressed file ...
+        with gzip.open(fname, mode = "rb") as gzObj:
+            # Open image as RGB (even if it is paletted) ...
+            with PIL.Image.open(gzObj) as iObj:
+                im = iObj.convert("RGB")
+    else:
+        # Open image as RGB (even if it is paletted) ...
+        with PIL.Image.open(fname) as iObj:
+            im = iObj.convert("RGB")
 
     # Return answer ...
     return im.size
