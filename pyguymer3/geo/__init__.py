f5cbfc887db9c76782740bd0ba35fd3b1beed880
Thomas Guymer <t.m.guymer@thomasguymer.co.uk>
2024-04-07 13:02:13 +0100

    hide two wrapper functions

--- pyguymer3/geo/__init__.py
+++ pyguymer3/geo/__init__.py
@@ -113,6 +113,7 @@ from ._add_background import _add_background
 from ._add_bathymetry import _add_bathymetry
 from ._add_elevation import _add_elevation
 from ._add_glaciatedAreas import _add_glaciatedAreas
+from ._add_global_axis import add_global_axis
 from ._add_lakes import _add_lakes
 from ._add_land import _add_land
 from ._add_minorIslands import _add_minorIslands
@@ -121,6 +122,7 @@ from ._add_railroads import _add_railroads
 from ._add_reefs import _add_reefs
 from ._add_rivers import _add_rivers
 from ._add_roads import _add_roads
+from ._add_topDown_axis import add_topDown_axis
 from ._add_urbanAreas import _add_urbanAreas
 from ._area import _area
 from ._buffer_points_crudely import _buffer_points_crudely
@@ -129,11 +131,9 @@ from ._points2polys import _points2polys
 from .add_annotation import add_annotation
 from .add_axis import add_axis
 from .add_coastlines import add_coastlines
-from .add_global_axis import add_global_axis
 from .add_horizontal_gridlines import add_horizontal_gridlines
 from .add_map_background import add_map_background
 from .add_map_underlay import add_map_underlay
-from .add_topDown_axis import add_topDown_axis
 from .add_vertical_gridlines import add_vertical_gridlines
 from .area import area
 from .buffer import buffer
