310c11b7b798c986e9026baec12ad25a26af4c1c
Thomas Guymer <t.m.guymer@thomasguymer.co.uk>
2024-07-14 12:40:28 +0100

    pass "eps" and "nmax" down the stack

--- pyguymer3/geo/great_circle.py
+++ pyguymer3/geo/great_circle.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 # Define function ...
-def great_circle(lon1, lat1, lon2, lat2, /, *, debug = False, maxdist = None, npoint = None, prefix = ".", ramLimit = 1073741824):
+def great_circle(lon1, lat1, lon2, lat2, /, *, debug = False, eps = 1.0e-12, maxdist = None, nmax = 100, npoint = None, prefix = ".", ramLimit = 1073741824):
     """Calculate the great circle that connects two coordinates.
 
     This function reads in two coordinates (in degrees) on the surface of the
@@ -20,8 +20,12 @@ def great_circle(lon1, lat1, lon2, lat2, /, *, debug = False, maxdist = None, np
         the latitude of the second coordinate (in degrees)
     debug : bool, optional
         print debug messages (in metres)
+    eps : float, optional
+        the tolerance of the Vincenty formula iterations
     maxdist : float, optional
         the maximum distance between points along the great circle
+    nmax : int, optional
+        the maximum number of the Vincenty formula iterations
     npoint : int, optional
         the number of points along the great circle
     prefix : str, optional
@@ -114,6 +118,8 @@ def great_circle(lon1, lat1, lon2, lat2, /, *, debug = False, maxdist = None, np
                     points[ipoint - 1][1],
                     points[ipoint][0],
                     points[ipoint][1],
+                     eps = eps,
+                    nmax = nmax,
                 )                                                               # [m]
 
                 # Check if the distance is too large ...
