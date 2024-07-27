6e923399d62f8c23bb801d111be8ada4e51dd6dd
Thomas Guymer <t.m.guymer@thomasguymer.co.uk>
2024-07-27 11:47:35 +0100

    rename variable; add docstring

--- pyguymer3/media/return_dict_of_bluray_playlistsToStreams.py
+++ pyguymer3/media/return_dict_of_bluray_playlistsToStreams.py
@@ -2,6 +2,12 @@
 
 # Define function ...
 def return_dict_of_bluray_playlistsToStreams(dname, /):
+    """
+    This function uses the list of MPLS files to obtain all of the possible
+    playlists in a Blu-ray, then it calls "parse_MPLS_file" on each file to
+    determine the duration.
+    """
+
     # Import standard modules ...
     import glob
     import os
@@ -10,7 +16,7 @@ def return_dict_of_bluray_playlistsToStreams(dname, /):
     from .parse_MPLS_file import parse_MPLS_file
 
     # Initialize dictionary ...
-    playlist2streams = {}
+    ans = {}
 
     # Loop over playlist files ...
     for mpls in glob.glob(f"{dname}/BDMV/PLAYLIST/*.mpls"):
@@ -18,17 +24,21 @@ def return_dict_of_bluray_playlistsToStreams(dname, /):
         iPlaylist = int(os.path.basename(mpls).removesuffix(".mpls"))
 
         # Initialize list and parse the playlist file ...
-        playlist2streams[iPlaylist] = []
+        ans[f"{iPlaylist:d}"] = []
         info = parse_MPLS_file(dname, iPlaylist)
 
         # Loop over playlist items ...
         for playItem in info["PlayList"]["PlayItems"]:
-            # Create short-hands ...
-            duration = float(playItem["OUTTime"] - playItem["INTime"]) / 4.5e4  # [s]
-            iStream = int(playItem["ClipInformationFileName"])
-
             # Populate dictionary ...
-            playlist2streams[iPlaylist].append({iStream : duration})
+            # NOTE: According to https://github.com/lw/BluRay/wiki/PlayItem, the
+            #       "times" are in "number of samples" assuming a frequency of
+            #       45 kHz.
+            ans[f"{iPlaylist:d}"].append(
+                (
+                    f'{int(playItem["ClipInformationFileName"]):d}',
+                    float(playItem["OUTTime"] - playItem["INTime"]) / 4.5e4,    # [s],
+                )
+            )
 
     # Return answer ...
-    return playlist2streams
+    return ans
