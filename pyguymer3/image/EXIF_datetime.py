a37fc979b74691221e4c11d125dffee8e87b1e79
Thomas Guymer <t.m.guymer@thomasguymer.co.uk>
2025-02-21 19:09:20 +0000

    use the GPS data even if the GPS DOP is missing

--- pyguymer3/image/EXIF_datetime.py
+++ pyguymer3/image/EXIF_datetime.py
@@ -5,9 +5,10 @@ def EXIF_datetime(
     info,
     /,
     *,
-            debug = __debug__,
-    gpsSupersedes = True,
-           maxDOP = 10.0,
+     debug = __debug__,
+    maxDOP = 10.0,
+    useDOP = False,
+    useGPS = True,
 ):
     # NOTE: The Wikipedia article on DOP has a handy table on what are good or
     #       bad DOP values:
@@ -26,29 +27,50 @@ def EXIF_datetime(
             print("DEBUG: There isn't a \"EXIF\" key.")
         return False
 
-    # If GPS data is present then is the DOP good enough to use the date/time
-    # information ...
-    goodDOP = False
-    if gpsSupersedes and all(
-        [
-            "GPSDOP" in info["EXIF"],
-            "GPSDateStamp" in info["EXIF"],
-            "GPSTimeStamp" in info["EXIF"],
-        ]
-    ):
-        goodDOP = bool(float(info["EXIF"]["GPSDOP"]) <= maxDOP)
-        if not goodDOP and debug:
-            print("DEBUG: There is GPS data but the DOP is not good enough.")
+    # Set flag ...
+    actuallyUseGPS = False
 
-    # Check if the DOP is good enough (which itself can only be true if the user
-    # wants to use the GPS data and that there is GPS data present to use) ...
-    if goodDOP:
+    # Check if the user wants to use GPS data ...
+    if useGPS:
+        # Check if the user wants to use GPS DOP data ...
+        if useDOP:
+            # Check that all the required keys are present ...
+            if all(
+                [
+                    "GPSDOP" in info["EXIF"],
+                    "GPSDateStamp" in info["EXIF"],
+                    "GPSTimeStamp" in info["EXIF"],
+                ]
+            ):
+                # Set flag ...
+                actuallyUseGPS = bool(float(info["EXIF"]["GPSDOP"]) <= maxDOP)
+                if not actuallyUseGPS and debug:
+                    print("DEBUG: There is GPS data but the DOP is not good enough.")
+            elif debug:
+                print("DEBUG: There aren't all of the \"EXIF::GPSDateStamp\" key, the \"EXIF::GPSTimeStamp\" key and the \"EXIF::GPSDOP\" key.")
+        else:
+            # Check that all the required keys are present ...
+            if all(
+                [
+                    "GPSDateStamp" in info["EXIF"],
+                    "GPSTimeStamp" in info["EXIF"],
+                ]
+            ):
+                # Set flag ...
+                actuallyUseGPS = True
+            elif debug:
+                print("DEBUG: There aren't all of the \"EXIF::GPSDateStamp\" key and the \"EXIF::GPSTimeStamp\" key.")
+
+    # Check if GPS data is to be used ...
+    if actuallyUseGPS:
         # Determine date/time that the photo was taken (assuming that the GPS
         # data is in UTC) ...
         us = 0                                                                  # [μs]
         if "." in info["EXIF"]["GPSTimeStamp"]:
             us = info["EXIF"]["GPSTimeStamp"].split(".")[1]
             us = int(f"{us:<6s}".replace(" ", "0"))                             # [μs]
+        elif debug:
+            print("DEBUG: There isn't any subsecond informaton in the \"EXIF::GPSTimeStamp\" key.")
         ans = datetime.datetime.strptime(
             f'{info["EXIF"]["GPSDateStamp"]} {info["EXIF"]["GPSTimeStamp"].split(".")[0]}.{us:06d}',
             "%Y:%m:%d %H:%M:%S.%f",
@@ -66,8 +88,6 @@ def EXIF_datetime(
             info["EXIF"]["DateTimeOriginal"],
             "%Y:%m:%d %H:%M:%S",
         ).replace(tzinfo = datetime.UTC)
-        if ans.microsecond != 0:
-            raise Exception("the EXIF data has sub-second information, this is not possible given the strptime() string") from None
 
         # Assume that the offset is valid (if it is present) which means that
         # the date/time was in fact in the local time zone rather than in UTC ...
@@ -85,46 +105,45 @@ def EXIF_datetime(
         elif debug:
             print("DEBUG: There isn't a \"EXIF::OffsetTimeOriginal\" key or a \"EXIF::TimeZoneOffset\" key.")
 
-    # Apply the sub-second offset (if GPS data was not used and if it is
-    # present) ...
-    if ans.microsecond == 0 and "SubSecTimeOriginal" in info["EXIF"]:
-        match info["EXIF"]["SubSecTimeOriginal"]:
-            case int():
-                ans += datetime.timedelta(
-                    milliseconds = info["EXIF"]["SubSecTimeOriginal"],
-                )
-            case str():
-                match len(info["EXIF"]["SubSecTimeOriginal"]):
-                    case 1:
-                        ans += datetime.timedelta(
-                            microseconds = 100000 * int(info["EXIF"]["SubSecTimeOriginal"]),
-                        )
-                    case 2:
-                        ans += datetime.timedelta(
-                            microseconds = 10000 * int(info["EXIF"]["SubSecTimeOriginal"]),
-                        )
-                    case 3:
-                        ans += datetime.timedelta(
-                            microseconds = 1000 * int(info["EXIF"]["SubSecTimeOriginal"]),
-                        )
-                    case 4:
-                        ans += datetime.timedelta(
-                            microseconds = 100 * int(info["EXIF"]["SubSecTimeOriginal"]),
-                        )
-                    case 5:
-                        ans += datetime.timedelta(
-                            microseconds = 10 * int(info["EXIF"]["SubSecTimeOriginal"]),
-                        )
-                    case 6:
-                        ans += datetime.timedelta(
-                            microseconds = int(info["EXIF"]["SubSecTimeOriginal"]),
-                        )
-                    case _:
-                        raise Exception(f'\"{info["EXIF"]["SubSecTimeOriginal"]}\" is an unexpected length') from None
-            case _:
-                raise Exception(f'\"{repr(type(info["EXIF"]["SubSecTimeOriginal"]))}\" is an unexpected type') from None
-    elif debug:
-        print("DEBUG: There isn't a \"EXIF::SubSecTimeOriginal\" key.")
+        # Apply the sub-second offset (if it is present) ...
+        if "SubSecTimeOriginal" in info["EXIF"]:
+            match info["EXIF"]["SubSecTimeOriginal"]:
+                case int():
+                    ans += datetime.timedelta(
+                        milliseconds = info["EXIF"]["SubSecTimeOriginal"],
+                    )
+                case str():
+                    match len(info["EXIF"]["SubSecTimeOriginal"]):
+                        case 1:
+                            ans += datetime.timedelta(
+                                microseconds = 100000 * int(info["EXIF"]["SubSecTimeOriginal"]),
+                            )
+                        case 2:
+                            ans += datetime.timedelta(
+                                microseconds = 10000 * int(info["EXIF"]["SubSecTimeOriginal"]),
+                            )
+                        case 3:
+                            ans += datetime.timedelta(
+                                microseconds = 1000 * int(info["EXIF"]["SubSecTimeOriginal"]),
+                            )
+                        case 4:
+                            ans += datetime.timedelta(
+                                microseconds = 100 * int(info["EXIF"]["SubSecTimeOriginal"]),
+                            )
+                        case 5:
+                            ans += datetime.timedelta(
+                                microseconds = 10 * int(info["EXIF"]["SubSecTimeOriginal"]),
+                            )
+                        case 6:
+                            ans += datetime.timedelta(
+                                microseconds = int(info["EXIF"]["SubSecTimeOriginal"]),
+                            )
+                        case _:
+                            raise Exception(f'\"{info["EXIF"]["SubSecTimeOriginal"]}\" is an unexpected length') from None
+                case _:
+                    raise Exception(f'\"{repr(type(info["EXIF"]["SubSecTimeOriginal"]))}\" is an unexpected type') from None
+        elif debug:
+            print("DEBUG: There isn't a \"EXIF::SubSecTimeOriginal\" key.")
 
     # Return answer ...
     return ans
