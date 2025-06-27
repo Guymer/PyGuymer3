adf0f67659d49264c2561dc7c72d15d6d454573f
Thomas Guymer <t.m.guymer@thomasguymer.co.uk>
2025-06-27 21:13:39 +0100

    rename function

--- pyguymer3/image/__init__.py
+++ pyguymer3/image/__init__.py
@@ -14,6 +14,7 @@ References
 """
 
 # Import sub-functions ...
+from ._makePng import _makePng
 from .dict2exif import dict2exif
 from .dot2png import dot2png
 from .EXIF_datetime import EXIF_datetime
@@ -30,9 +31,9 @@ from .load_EXIF2 import load_EXIF2
 from .load_GPS_EXIF import load_GPS_EXIF
 from .load_GPS_EXIF1 import load_GPS_EXIF1
 from .load_GPS_EXIF2 import load_GPS_EXIF2
+from .manuallyOptimisePNG import manuallyOptimisePNG
 from .optimize_image import optimize_image
 from .optipng import optipng
-from .paeth_filter import paeth_filter
 from .return_image_size import return_image_size
 from .save_array_as_image import save_array_as_image
 from .save_array_as_PNG import save_array_as_PNG
--- pyguymer3/image/_makePngSrc/paethFilter.py
+++ pyguymer3/image/_makePngSrc/paethFilter.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 # Define function ...
-def paeth_filter(
+def paethFilter(
     a,
     b,
     c,
