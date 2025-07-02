009a30dd399637c796c1e9db6469e3545a3932a9
Thomas Guymer <t.m.guymer@thomasguymer.co.uk>
2025-07-02 17:28:28 +0100

    support all three image modes

--- pyguymer3/image/manuallyOptimisePng.py
+++ pyguymer3/image/manuallyOptimisePng.py
@@ -79,15 +79,39 @@ def manuallyOptimisePng(
 
     # **************************************************************************
 
-    # Open image as RGB (even if it is paletted) ...
+    # Open image and make arrays ...
     with PIL.Image.open(pName) as iObj:
-        img = iObj.convert("RGB")
+        match iObj.mode:
+            case "L":
+                arrUint8 = numpy.array(iObj).reshape((iObj.size[1], iObj.size[0], 1))
+                palUint8 = None
+            case "P":
+                arrUint8 = numpy.array(iObj).reshape((iObj.size[1], iObj.size[0], 1))
+                palUint8 = numpy.frombuffer(iObj.palette.tobytes(), dtype = numpy.uint8)
+                palUint8 = palUint8.reshape((palUint8.size // 3, 3))
+            case "RGB":
+                arrUint8 = numpy.array(iObj)
+                palUint8 = None
+            case _:
+                raise ValueError(f"the image has an unsupported mode (\"{iObj.mode}\")") from None
 
-    # Convert image to a NumPy array ...
-    arrUint8 = numpy.array(img)
+    # Check soon-to-be input ...
     assert arrUint8.dtype == "uint8", f"the NumPy array is not 8-bit (\"{arrUint8.dtype}\")"
     assert arrUint8.ndim == 3, f"the NumPy array is not 3D (\"{arrUint8.ndim:d}\")"
-    assert arrUint8.shape[2] == 3, "the NumPy array does not have 3 colour channels"
+    match arrUint8.shape[2]:
+        case 1:
+            if palUint8 is None:
+                pass
+            else:
+                assert palUint8.dtype == "uint8", f"the NumPy palette is not 8-bit (\"{palUint8.dtype}\")"
+                assert palUint8.ndim == 2, f"the NumPy palette is not 2D (\"{palUint8.ndim:d}\")"
+                assert palUint8.shape[0] <= 256, f"the NumPy palette has more than 256 colours (\"{palUint8.shape[0]:,d}\")"
+                assert palUint8.shape[1] == 3, "the NumPy palette does not have 3 colour channels"
+                assert arrUint8.max() < palUint8.shape[0], f"the NumPy array references more colours than are in the NumPy palette (\"{arrUint8.max():d}\" -vs- \"{palUint8.shape[0]:d}\")"
+        case 3:
+            pass
+        case _:
+            raise ValueError(f"the NumPy array does not have either 1 or 3 colour channels (\"{arrUint8.shape[2]:d}\")") from None
 
     # Make PNG source ...
     src = makePng(
@@ -104,7 +128,7 @@ def manuallyOptimisePng(
               levels = [9,],
            memLevels = [9,],
              modTime = modTime,
-            palUint8 = None,
+            palUint8 = palUint8,
           strategies = None,
               wbitss = [15,],
     )
@@ -112,6 +136,9 @@ def manuallyOptimisePng(
     # Check if the new source is smaller than the old source ...
     if len(src) >= os.path.getsize(pName):
         return
+
+    if debug:
+        print(f"Overwriting \"{pName}\" with optimised version ({len(src):,d} bytes < {os.path.getsize(pName):,d} bytes) ...")
 
     # Write PNG ...
     with open(pName, "wb") as fObj:
