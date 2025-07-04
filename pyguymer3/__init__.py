7c1733baf26f0fb487e38f44ae017a436ca9dbc3
Thomas Guymer <t.m.guymer@thomasguymer.co.uk>
2025-07-04 12:02:24 +0100

    tell the user about the new features

--- pyguymer3/__init__.py
+++ pyguymer3/__init__.py
@@ -2,7 +2,9 @@
 
 """
 A Python module containing a bunch of random functions that I have written over
-the years.
+the years. If you want to use some of these functions directly from the command
+line then you can run ``python3.13 -m pyguymer3 --help`` to see what is
+available.
 
 Notes
 -----
