db18e3301ea27ab1f8b780ec7266d4cf1c87b59e
Thomas Guymer <t.m.guymer@thomasguymer.co.uk>
2025-08-02 08:15:31 +0100

    use a reference instead

--- pyguymer3/openstreetmap/__init__.py
+++ pyguymer3/openstreetmap/__init__.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 
 """
-A Python sub-module containing functions relating to
-`OpenStreetMap <https://www.openstreetmap.org>`_.
+A Python sub-module containing functions relating to`OpenStreetMap [2]_.
 
 Notes
 -----
@@ -11,6 +10,7 @@ Copyright 2017 Thomas Guymer [1]_
 References
 ----------
 .. [1] PyGuymer3, https://github.com/Guymer/PyGuymer3
+.. [2] OpenStreetMap, https://www.openstreetmap.org
 """
 
 # Import sub-functions ...
