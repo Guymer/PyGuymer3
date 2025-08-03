f0002d37a992347a81aac1ca3c6e576b033d6e22
Thomas Guymer <t.m.guymer@thomasguymer.co.uk>
2025-08-03 10:53:09 +0100

    allow users to save paletted images

--- pyguymer3/image/save_array_as_PNG.py
+++ pyguymer3/image/save_array_as_PNG.py
@@ -2,7 +2,7 @@
 
 # Define function ...
 def save_array_as_PNG(
-    img,
+    arrUint8,
     fname,
     /,
     *,
@@ -15,13 +15,14 @@ def save_array_as_PNG(
            debug: bool = __debug__,
              dpi: None | int = None,
          modTime = None,
+        palUint8 = None,
 ):
     """Save an array as a PNG image.
 
     Parameters
     ----------
-    img : numpy.ndarray
-        a 3D NumPy array of uint8 type with shape (ny,nx,nc)
+    arrUint8 : numpy.ndarray
+        A "height * width * colour" unsigned 8-bit integer NumPy array.
     fname : str
         output file name
     calcAdaptive : bool, optional
@@ -41,6 +42,8 @@ def save_array_as_PNG(
     dpi : None or float or int, optional
         See :py:func:`pyguymer3.image.makePng` for the documentation.
     modTime : None or datetime.datetime, optional
+        See :py:func:`pyguymer3.image.makePng` for the documentation.
+    palUint8 : None or numpy.ndarray, optional
         See :py:func:`pyguymer3.image.makePng` for the documentation.
 
     Notes
@@ -64,15 +67,28 @@ def save_array_as_PNG(
     assert sys.byteorder == "little", "the system is not little-endian"
 
     # Check input ...
-    assert img.dtype == "uint8", f"the NumPy array is not 8-bit (\"{img.dtype}\")"
-    assert img.ndim == 3, f"the NumPy array is not 3D (\"{img.ndim:d}\")"
-    assert img.shape[2] == 3, "the NumPy array does not have 3 colour channels"
+    assert arrUint8.dtype == "uint8", f"the NumPy array is not 8-bit (\"{arrUint8.dtype}\")"
+    assert arrUint8.ndim == 3, f"the NumPy array is not 3D (\"{arrUint8.ndim:d}\")"
+    match arrUint8.shape[2]:
+        case 1:
+            if palUint8 is None:
+                pass
+            else:
+                assert palUint8.dtype == "uint8", f"the NumPy palette is not 8-bit (\"{palUint8.dtype}\")"
+                assert palUint8.ndim == 2, f"the NumPy palette is not 2D (\"{palUint8.ndim:d}\")"
+                assert palUint8.shape[0] <= 256, f"the NumPy palette has more than 256 colours (\"{palUint8.shape[0]:,d}\")"
+                assert palUint8.shape[1] == 3, "the NumPy palette does not have 3 colour channels"
+                assert arrUint8.max() < palUint8.shape[0], f"the NumPy array references more colours than are in the NumPy palette (\"{arrUint8.max():d}\" -vs- \"{palUint8.shape[0]:d}\")"
+        case 3:
+            pass
+        case _:
+            raise ValueError(f"the NumPy array does not have either 1 or 3 colour channels (\"{arrUint8.shape[2]:d}\")") from None
 
     # **************************************************************************
 
     # Make PNG source ...
     src = makePng(
-        img,
+        arrUint8,
         calcAdaptive = calcAdaptive,
          calcAverage = calcAverage,
             calcNone = calcNone,
@@ -85,7 +101,7 @@ def save_array_as_PNG(
               levels = [9,],
            memLevels = [9,],
              modTime = modTime,
-            palUint8 = None,
+            palUint8 = pal,
           strategies = None,
               wbitss = [15,],
     )
