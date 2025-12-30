9bd76f6b9799a00b2c278b10856d6c25cc6e2ca3
Thomas Guymer <t.m.guymer@thomasguymer.co.uk>
2025-12-30 09:24:58 +0000

    typo

--- pyguymer3/osterrain/__init__.py
+++ pyguymer3/osterrain/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 """
-A Python sub-module containing functions relating to the OS Terrain 50 dataset
+A Python sub-module containing functions relating to the "OS Terrain 50" dataset
 [2]_.
 
 Notes
@@ -11,7 +11,7 @@ Copyright 2017 Thomas Guymer [1]_
 References
 ----------
 .. [1] PyGuymer3, https://github.com/Guymer/PyGuymer3
-.. [2] OS Terrain 50 dataset, https://www.ordnancesurvey.co.uk/products/os-terrain-50
+.. [2] "OS Terrain 50" dataset, https://www.ordnancesurvey.co.uk/products/os-terrain-50
 """
 
 # Import sub-functions ...
