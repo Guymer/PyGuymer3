4a82b5f766546684adc0aadc57981deb6ec00eba
Thomas Guymer <t.m.guymer@thomasguymer.co.uk>
2025-12-30 12:05:19 +0000

    only use defaults

--- scripts/generateTiles.sh
+++ scripts/generateTiles.sh
@@ -1,13 +1,7 @@
 #!/usr/bin/env bash
 
 # Run scripts ...
-python3.11 generateGlobeTiles.py                                                \
-    --maximum-elevation-interval 1000
-python3.11 generateGshhgTiles.py                                                \
-    --resolutions c l i h f
-python3.11 generateNeTiles.py                                                   \
-    --elevation-band-interval 250                                               \
-    --maximum-elevation-interval 1000                                           \
-    --resolutions 10m 50m 110m
-python3.11 generateOsTerrainTiles.py                                            \
-    --maximum-elevation-interval 250
+python3.11 generateGlobeTiles.py --number-of-children 4
+python3.11 generateGshhgTiles.py --number-of-children 4
+python3.11 generateNeTiles.py --number-of-children 4
+python3.11 generateOsTerrainTiles.py --number-of-children 4
