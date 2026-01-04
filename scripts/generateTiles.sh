b10f7ad3c59ae2fca5ecc3fac5d3d23286264972
Thomas Guymer <t.m.guymer@thomasguymer.co.uk>
2026-01-04 08:06:22 +0000

    make tiles in a different order

--- scripts/generateTiles.sh
+++ scripts/generateTiles.sh
@@ -1,8 +1,20 @@
 #!/usr/bin/env bash
 
-# Run scripts ...
-python3.11 generateGlobeTiles.py &> generateGlobeTiles.log
-python3.11 generateGlobeNeTiles.py &> generateGlobeNeTiles.log
-python3.11 generateGshhgTiles.py &> generateGshhgTiles.log
-python3.11 generateNeTiles.py &> generateNeTiles.log
-python3.11 generateOsTerrainTiles.py &> generateOsTerrainTiles.log
+# Create short-hand ...
+NCHILD=6                                                                        # [#]
+
+# Generate vector tiles ...
+python3.11 generateGshhgTiles.py                                                \
+    --number-of-children ${NCHILD} &> generateGshhgTiles.log
+python3.11 generateNeTiles.py                                                   \
+    --number-of-children ${NCHILD} &> generateNeTiles.log
+
+# Generate raster tiles ...
+python3.11 generateGlobeTiles.py                                                \
+    --number-of-children ${NCHILD} &> generateGlobeTiles.log
+python3.11 generateOsTerrainTiles.py                                            \
+    --number-of-children ${NCHILD} &> generateOsTerrainTiles.log
+
+# Generate combined raster+vector tiles ...
+python3.11 generateGlobeNeTiles.py                                              \
+    --number-of-children ${NCHILD} &> generateGlobeNeTiles.log
